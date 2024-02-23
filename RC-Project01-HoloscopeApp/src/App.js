ee5388b8f7441c0309f76fb8b14fd220cd90915b
Abdulxd <tartilaci@gmail.com>
2024-02-23 10:07:09 +0100

    add files

---
+++ RC-Project01-HoloscopeApp/src/App.js
@@ -0,0 +1,20 @@
+//! burada kullanılacak tüm sayfalar üste import edilmek ZORUNDA!
+
+
+
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+    
+    
+    App    
+    
+    
+    </div>
+  )
+}
+
+export default App
+
---
+++ RC-Project01-HoloscopeApp/src/components/header/Header.jsx
@@ -0,0 +1,10 @@
+import './Header.scss'
+
+
+const Header = () => {
+  return (
+    <header>Header</header>
+  )
+}
+
+export default Header
