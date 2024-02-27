48a8383263e990cf053123b9a4c18ddfa5602fe5
Abdulxd <tartilaci@gmail.com>
2024-02-27 11:09:51 +0100

    learn events and hooks

--- RC-01-Starter - Kopie/src/1-events-hookintro/Events.jsx
+++ RC-01-Starter - Kopie/src/1-events-hookintro/Events.jsx
@@ -4,6 +4,8 @@ const Events = () => {
 
     // js alani
 
+    
+
     let baslik ="merhaba";
     let count = 0;
 
@@ -23,6 +25,10 @@ const Events = () => {
         console.log(a);
         document.querySelector("h1").textContent=a
          }
+
+
+
+           //! react alanında parametreli fonksiyon çağıracaksak, fonksiyonun adının önüne ()=> işaretini koymalıyız, yoksa fonksiyonu event ı beklemeden çalıştırıyor
   return (
     // jsx alani
     <div className="container text-center mt-4">
@@ -30,21 +36,21 @@ const Events = () => {
 
     <h2>  Count: <span className="text-success">{count}</span> </h2>
 
-    //! react alanında parametreli fonksiyon çağıracaksak, fonksiyonun adının önüne ()=> işaretini koymalıyız, yoksa fonksiyonu event ı beklemeden çalıştırıyor
+  
 
 
     <button onClick={arttir} className='btn btn-primary'>ARTTIR</button>
     <button onClick={()=>changeTitle("naber")} className='btn btn-danger'>BASLIK DEGISTIR</button>
     <button className='btn btn-warning'>TIKLANDI</button>
-    //?-------------------------------------------------------------------
-//! Konsolda, güncellenen değişen count u görebiliriz ancak web sayfasında (biz görüntüle demeden) görüntülenmiyor..
+
+
+{/* //! Konsolda, güncellenen değişen count u görebiliriz ancak web sayfasında (biz görüntüle demeden) görüntülenmiyor..
 //* Çünkü, REACT herhangi bir element i default olarak static sayar
 //*DOM manipülasyonunu en aza indirmek için bunu yapar
 //* Hangi elementin interactive olduğu konusunda React'e bilgi vermeliyiz.
 //! REACT'i interactive elements hakkında bilgilendirmek için iki yöntem kullanıyoruz
 //* 1. Statefull Classes. State-based inform (Class Components)
-//* 2. Hooks (Functional Components)
-//?--------------------------------------------------------------------
+//* 2. Hooks (Functional Components) */}
 
     </div>
   )
--- RC-01-Starter - Kopie/src/App.js
+++ RC-01-Starter - Kopie/src/App.js
@@ -2,14 +2,16 @@
 
 
 import React from 'react'
-import Events from './1-events-hookintro/Events'
+// import Events from './1-events-hookintro/Events'
 import "bootstrap/dist/css/bootstrap.min.css";
+import Hooks from './2-hooks/Hooks';
 
 const App = () => {
   return (
     <div>
 
-    <Events/>
+    {/* <Events/> */}
+    <Hooks/>
     
     
     
