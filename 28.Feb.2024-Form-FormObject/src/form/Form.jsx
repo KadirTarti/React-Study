dc7d6acf3a1f44588e500460ffb09e04e8288533
Abdulxd <tartilaci@gmail.com>
2024-02-28 20:00:00 +0100

    form finish

--- 28.Feb.2024-Form-FormObject/src/form/Form.jsx
+++ 28.Feb.2024-Form-FormObject/src/form/Form.jsx
@@ -8,16 +8,19 @@ const Form = () => {
 
   const getDataBase = (e) => {
     e.preventDefault()
+
     //? ileride bir submit islemin neticesinde verilerin nasıl post edilebileceğini gostermek adina eklenmistir.
     //? const res = axios.post("url", { isim, pass, country })
 
     alert(`name: ${isim}
            password: ${pass}
            ülkeler: ${country}`);
-           setIsim=''
-           setPass=''
-           setCountry=''
-  }
+
+           //! database'e yollandıktan sonra değişkenlerin içini temizledik bunu ekranda da temiz görmek için inputlarda value değişkenleri tanımladık.
+           setIsim('')
+           setPass('')
+           setCountry('')
+  };
 
   return (
     <div className="container text-center mt-4">
@@ -28,42 +31,47 @@ const Form = () => {
       <form onSubmit={getDataBase}>
         <div className="mb-3">
           <label className="form-label" htmlFor="isim">
-        NAME: <span className='text-danger'> {isim} </span>
+            NAME: <span className="text-danger"> {isim} </span>
           </label>
-          <input className="form-control" id="isim" type="text" 
-            onInput={(e)=>setIsim(e.target.value)}
-      value={isim}
+          <input
+            className="form-control"
+            id="isim"
+            type="text"
+            onInput={(e) => setIsim(e.target.value)}
+            value={isim}
           />
         </div>
 
-
-
         <div className="mb-3">
-          <label className="form-label" htmlFor="password">PASSWORD</label>
-          <input className="form-control" id="password" type="password"
-          onChange={(e)=>setPass(e.target.value)}
-      value={pass}     />
+          <label className="form-label" htmlFor="password">
+            PASSWORD
+          </label>
+          <input
+            className="form-control"
+            id="password"
+            type="password"
+            onChange={(e) => setPass(e.target.value)}
+            value={pass}
+          />
         </div>
-
-
-
 
         <div className="mb-3">
           <label className="form-label" htmlFor="country">
-         COUNTRY: <span className='text-success'>{country}</span>
+            COUNTRY: <span className="text-success">{country}</span>
           </label>
-         <select 
-        onChange={(e)=>setCountry(e.target.value)        } 
-          className="form-select">
+          <select
+            onChange={(e) => setCountry(e.target.value)}
+            className="form-select"
+          >
             <option selected>COUNTRIES</option>
-            <option value='Türkiye' >TURKEY</option>
-            <option value='Almanya' >GERMANY</option>
-            <option value='Amerika' >USA</option>
-         </select>
-
+            <option value="Türkiye">TURKEY</option>
+            <option value="Almanya">GERMANY</option>
+            <option value="Amerika">USA</option>
+          </select>
         </div>
-<button type="submit" className="btn btn-primary">GÖNDER (submit)</button>
-
+        <button type="submit" className="btn btn-primary">
+          GÖNDER (submit)
+        </button>
       </form>
     </div>
   );
