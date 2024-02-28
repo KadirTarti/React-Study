8e2b55a96c3c5ab716d8fadab748d03269ed813b
Abdulxd <tartilaci@gmail.com>
2024-02-28 20:12:51 +0100

    start formObject

--- 28.Feb.2024-Form-FormObject/src/App.js
+++ 28.Feb.2024-Form-FormObject/src/App.js
@@ -2,14 +2,16 @@
 
 
 import React from 'react'
-import Form from './form/Form'
+// import Form from './form/Form'
 import "bootstrap/dist/css/bootstrap.min.css";
+import FormObject from './form/FormObject';
 
 const App = () => {
   return (
     <div>
 
-    <Form/>
+    {/* <Form/> */}
+    <FormObject/>
     
     </div>
   )
--- 28.Feb.2024-Form-FormObject/src/form/FormObject.jsx
+++ 28.Feb.2024-Form-FormObject/src/form/FormObject.jsx
@@ -1,9 +1,66 @@
-import React from 'react'
+import { useState } from "react";
 
 const FormObject = () => {
-  return (
-    <div>FormObject</div>
-  )
-}
 
-export default FormObject
+
+  const getDataBase = (e) => {
+    e.preventDefault()
+
+  };
+
+  return (
+    <div className="container text-center mt-4">
+      <h1>*********************************************</h1>
+
+      <h1>FORM(EVENTS)</h1>
+
+      <form onSubmit={getDataBase}>
+        <div className="mb-3">
+          <label className="form-label" htmlFor="isim">
+            NAME: <span className="text-danger"></span>
+          </label>
+          <input
+            className="form-control"
+            id="isim"
+            type="text"
+            // onInput={(e) => setIsim(e.target.value)}
+            // value={isim} 
+          />
+        </div>
+
+        <div className="mb-3">
+          <label className="form-label" htmlFor="password">
+            PASSWORD
+          </label>
+          <input
+            className="form-control"
+            id="password"
+            type="password"
+            // onChange={(e) => setPass(e.target.value)}
+            // value={pass}
+          />
+        </div>
+
+        <div className="mb-3">
+          <label className="form-label" htmlFor="country">
+            COUNTRY: <span className="text-success"></span>
+          </label>
+          <select
+            // onChange={(e) => setCountry(e.target.value)}
+            className="form-select"
+          >
+            <option selected>COUNTRIES</option>
+            <option value="Türkiye">TURKEY</option>
+            <option value="Almanya">GERMANY</option>
+            <option value="Amerika">USA</option>
+          </select>
+        </div>
+        <button type="submit" className="btn btn-primary">
+          GÖNDER (submit)
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default FormObject;
