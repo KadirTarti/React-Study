36039dfd13861b2d46c855a9425d84cb9c7050dc
Abdulxd <tartilaci@gmail.com>
2024-02-28 20:34:52 +0100

    use same function for all inputs

--- 28.Feb.2024-Form-FormObject/src/form/FormObject.jsx
+++ 28.Feb.2024-Form-FormObject/src/form/FormObject.jsx
@@ -1,7 +1,19 @@
 import { useState } from "react";
 
 const FormObject = () => {
+  
+  const [person, setPerson] = useState({
+    isim: '',
+    password: '',
+    email: ''
+  })
 
+  //! destructing
+  const {isim, password, email}=person
+
+  const handlePerson =(event)=>{
+    setPerson({...person, [event.target.id]: event.target.value})
+  }
 
   const getDataBase = (e) => {
     e.preventDefault()
@@ -17,13 +29,13 @@ const FormObject = () => {
       <form onSubmit={getDataBase}>
         <div className="mb-3">
           <label className="form-label" htmlFor="isim">
-            NAME: <span className="text-danger"></span>
+            NAME: <span className="text-danger">{isim}</span>
           </label>
           <input
             className="form-control"
             id="isim"
             type="text"
-            // onInput={(e) => setIsim(e.target.value)}
+            onInput={handlePerson}
             // value={isim} 
           />
         </div>
@@ -34,20 +46,21 @@ const FormObject = () => {
           </label>
           <input
             className="form-control"
-            id="password"
+            id="password" //! hepsine id verdiğimiz için handlePerson içinde event.target.id ile her birini yakalayabiliriz
             type="password"
-            // onChange={(e) => setPass(e.target.value)}
+            onChange={handlePerson}
             // value={pass}
           />
         </div>
 
         <div className="mb-3">
           <label className="form-label">
-            EMAIL: <span className="fw-bold">{}</span>
+            EMAIL: <span className="fw-bold">{email}</span>
           </label>
           <input
             type="email"
             className="form-control"
+            onChange={handlePerson}
             // value={email}
             id="email"
             name="email"
