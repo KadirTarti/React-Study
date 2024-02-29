f9c9f894c902072ca3095945159fd1aaf1afa3a1
Abdulxd <tartilaci@gmail.com>
2024-03-01 00:54:12 +0100

    add input properties

--- 29.Feb.2024-Guided_Workshop_NBA-Stars/src/components/Header.jsx
+++ 29.Feb.2024-Guided_Workshop_NBA-Stars/src/components/Header.jsx
@@ -1,11 +1,45 @@
+import { useState } from 'react'
 import logo from '../assets/nba-logo.png'
+import {data} from '../helper/data'
 
 const Header = () => {
+  const [query, setQuery] = useState('');
+  const [filteredPlayers, setFilteredPlayers] = useState([]);
+
+
+  const filterPlayers = (searchQuery) => {
+    if (!searchQuery) {
+      setFilteredPlayers([]);
+      return;
+    }
+    const results = data.filter(player =>
+      player.name.toLowerCase().includes(searchQuery.toLowerCase())
+    );
+    setFilteredPlayers(results);
+ };
+
+ const handleChange = (e) => {
+  setQuery(e.target.value);
+  filterPlayers(e.target.value);
+};
+
   return (
+
+
     <div className='text-center mt-4'>
-    <img   src={logo} alt="" />
+    <img   src={logo} alt="nba logo" />
     <h1 className='mt-4'>NBA Legends</h1>
-    <input type="text" placeholder='Search Player...' />
+    <input type="text" 
+    value={query}  
+    onChange={handleChange}
+    placeholder='Search Player...' />
+    
+    <div>
+        {filteredPlayers.map(player => (
+          <div key={player.id}>{player.name}</div>
+        ))}
+      </div>
+ 
     </div>
   )
 }
