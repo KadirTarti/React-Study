b791f6672b4838a5ed089e33657812768ba37b5b
Abdulxd <tartilaci@gmail.com>
2024-02-29 12:17:48 +0100

    add scss properties

--- 29.Feb.2024-Guided_Workshop_NBA-Stars/src/components/PlayerCard.jsx
+++ 29.Feb.2024-Guided_Workshop_NBA-Stars/src/components/PlayerCard.jsx
@@ -1,21 +1,35 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './playercard.scss';
 
 
 const PlayerCard = (props) => {
   
   const {name, img, statistics} = props
-  return (
-    <div className='mt-4'>
 
-      <div className="card" style={{ width: "18rem" }}>
-        <img className="card-img-top" src={img} alt="Card image cap" />
-        <div className="card-body">
-        <h5>{name}</h5>
+    const [isFlipped, setIsFlipped] = useState(false);
+
+    const handleClick = ()=>{
+      setIsFlipped(!isFlipped);
+    }
+
+  
+  return (
+    <div className='player-card mt-4'>
+
+      <div className={`card ${isFlipped ? 'flipped' : ''}`} onClick={handleClick}>
+        <div className="card-inner">
+          <div className="card-front">
+            <img className="card-img-top" src={img} alt="Player" />
+              <div className="card-body">
+              <h5>{name}</h5>
+              </div>
+          </div>
+        <div className="card-back">
+          <ul className='d-flex justify-content-center align-items-center'>{statistics}</ul>
+        </div>
         </div>
       </div>    
-    
-    </div>
+        </div>
   )
 }
 
