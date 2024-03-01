5422fe38dfe6ca28ac1ac1b81028eab26a576168
Abdulxd <tartilaci@gmail.com>
2024-03-01 14:20:39 +0100

    add files

---
+++ 01.03.2024-Olympcs-LEGENDS-App/src/components/CardContainer.jsx
@@ -0,0 +1,38 @@
+import React, { useState } from "react";
+import { data } from "../helpers/data";
+import { Col, Container, Form, Row } from "react-bootstrap";
+import PlayerCard from "./PlayerCard";
+
+const CardContainer = () => {
+    const [search, setSearch] = useState('')
+    // let dizi=data;
+
+    //& 1.yol
+    // dizi=dizi.filter((a)=>a.name.includes(search))
+
+  return (
+    <>
+      <Form.Control className='w-50 m-auto' type="search" placeholder="Search Player" 
+        onChange={(e)=>setSearch(e.target.value)}
+
+      />
+
+      <Container className="card-container p-3 rounded-4 my-4">
+        <Row className="g-3">
+
+        {/* search e isim girince filtreleme: */}
+          {data.filter((a)=>a.name.toLowerCase().includes(search.trim().toLowerCase())).map((player, index) => {
+            return (
+              <Col key={index} md={6} lg={4} xl={3}>
+                {/* <PlayerCard player = {player}></PlayerCard> */}
+                <PlayerCard {...player}></PlayerCard>
+              </Col>
+            );
+          })}
+        </Row>
+      </Container>
+    </>
+  );
+};
+
+export default CardContainer;
