f359107a2c15fb2a50f6f0e3e31f70b7255254a6
Abdulxd <tartilaci@gmail.com>
2024-03-01 11:18:13 +0100

    card container dizi/ filter

--- 01.03.2024-NBA-Workshop-PROJECT-withAshley/src/components/CardContainer.jsx
+++ 01.03.2024-NBA-Workshop-PROJECT-withAshley/src/components/CardContainer.jsx
@@ -1,15 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import { data } from "../helpers/data";
 import { Col, Container, Form, Row } from "react-bootstrap";
 import PlayerCard from "./PlayerCard";
 
 const CardContainer = () => {
+    const [search, setSearch] = useState('')
+    let dizi=data;
+    dizi=dizi.filter((a)=>a.name.includes(search))
+
   return (
     <>
-      <Form.Control type="search" placeholder="Search Player" />
+      <Form.Control className='w-50 m-auto' type="search" placeholder="Search Player" 
+        onChange={(e)=>setSearch(e.target.value)}
+
+      />
+
       <Container className="card-container p-3 rounded-4 my-4">
         <Row className="g-3">
-          {data.map((player, index) => {
+          {dizi.map((player, index) => {
             return (
               <Col key={index} md={6} lg={4} xl={3}>
                 {/* <PlayerCard player = {player}></PlayerCard> */}
