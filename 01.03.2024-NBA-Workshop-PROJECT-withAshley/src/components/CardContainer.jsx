a109f007769a56023bb69498cdf20548c1249f6b
Abdulxd <tartilaci@gmail.com>
2024-03-01 10:58:43 +0100

    finish playercard

--- 01.03.2024-NBA-Workshop-PROJECT-withAshley/src/components/CardContainer.jsx
+++ 01.03.2024-NBA-Workshop-PROJECT-withAshley/src/components/CardContainer.jsx
@@ -1,44 +1,26 @@
-import React from 'react';
-import {data} from '../helpers/data';
-import { Col, Container, Form, Row } from 'react-bootstrap';
-import PlayerCard from './PlayerCard';
-
+import React from "react";
+import { data } from "../helpers/data";
+import { Col, Container, Form, Row } from "react-bootstrap";
+import PlayerCard from "./PlayerCard";
 
 const CardContainer = () => {
-    
   return (
-
-    <div>
-
-    <Form.Control type="email" placeholder="Search Player" />    
-    <Container>
-        <Row>
-
-            {data.map((player, index)=>{
-                return (
-                    <Col key={index}>
-                    {/* <PlayerCard player = {player}></PlayerCard> */}
-                    <PlayerCard {...player}>
-                        
-                    </PlayerCard>
-                    </Col>
-                                )
-
-                
-
-
-
-            })}
+    <>
+      <Form.Control type="search" placeholder="Search Player" />
+      <Container className="card-container p-3 rounded-4 my-4">
+        <Row className="g-3">
+          {data.map((player, index) => {
+            return (
+              <Col key={index} md={6} lg={4} xl={3}>
+                {/* <PlayerCard player = {player}></PlayerCard> */}
+                <PlayerCard {...player}></PlayerCard>
+              </Col>
+            );
+          })}
         </Row>
-    </Container>
+      </Container>
+    </>
+  );
+};
 
-    
-
-
-
-    
-    </div>
-  )
-}
-
-export default CardContainer
+export default CardContainer;
