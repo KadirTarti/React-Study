203baed1250cc961425a90325cb582dccfe64c0f
Abdulxd <tartilaci@gmail.com>
2024-03-02 21:08:03 +0100

    add header - search bar

--- 02.03.2024-RC-toDo-APP/src/App.js
+++ 02.03.2024-RC-toDo-APP/src/App.js
@@ -1,15 +1,18 @@
 import './App.css';
 //! burada kullanılacak tüm sayfalar üste import edilmek ZORUNDA!
 
-
+import '../node_modules/bootstrap/dist/css/bootstrap.css'
 import React from 'react'
+import Header from './Components/Header';
+import TodoList from './Components/TodoList';
 
 const App = () => {
   return (
     <div>
     
     
-    App    
+    <Header/>
+    <TodoList/>
     
     
     </div>
--- 02.03.2024-RC-toDo-APP/src/Components/Header.jsx
+++ 02.03.2024-RC-toDo-APP/src/Components/Header.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
+import { Button, Form, InputGroup } from 'react-bootstrap'
+
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <div className='container'>
+    <h1 className='text-danger'>Todo App</h1>
+    <InputGroup className="mb-3">
+        <Form.Control
+          placeholder="Recipient's username"
+          aria-label="Recipient's username"
+          aria-describedby="basic-addon2"
+        />
+        <Button type="button" className="btn btn-success text-light" variant="outline-secondary" id="button-addon2">
+          Add Todo
+        </Button>
+      </InputGroup>
+    </div>
   )
 }
 
