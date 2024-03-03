92ccdfd193d06241f598889c4191ab0754d15d80
Abdulxd <tartilaci@gmail.com>
2024-03-03 19:54:36 +0100

    add files

---
+++ zProjects/02.03.2024-RC-toDo-APP--2/src/TodoList.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const TodoList = () => {
+  return (
+    <div>
+        <form action="">
+            <input type="text" />
+            <button>Add Todo</button>
+        </form>
+
+        <ul>
+            <li></li>
+            <li></li>
+        </ul>
+    </div>
+  )
+}
+
+export default TodoList
