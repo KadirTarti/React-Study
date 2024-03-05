2ff8d01d899b61d6fc4a9d7531fb5c46362ffa4b
KadirTarti <tartilaci@gmail.com>
2024-03-05 09:29:14 +0100

    add files

---
+++ 05.03.2024-RC-toDoApp-wAshleyy/src/components/GorevleriGoster.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { FaTimesCircle } from "react-icons/fa";
+const GorevleriGoster = () => {
+
+
+
+  return (
+    <div>
+    
+    </div>
+  );
+};
+
+export default GorevleriGoster;
---
+++ 05.03.2024-RC-toDoApp-wAshleyy/src/pages/Home.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import GorevEkle from "../components/GorevEkle";
+import GorevleriGoster from "../components/GorevleriGoster";
+
+import Data from '../helper/Data'
+
+const Home = () => {
+
+
+ 
+  return (
+    <div>
+          
+    </div>
+  );
+};
+
+export default Home;
