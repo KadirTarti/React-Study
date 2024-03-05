10e8cf8a745b79577d913dd7d951ff60efeccc95
KadirTarti <tartilaci@gmail.com>
2024-03-05 10:34:47 +0100

    görevler, göster tamamlandı

--- 05.03.2024-RC-toDoApp-wAshleyy/src/components/GorevleriGoster.jsx
+++ 05.03.2024-RC-toDoApp-wAshleyy/src/components/GorevleriGoster.jsx
@@ -3,6 +3,10 @@ import { FaTimesCircle } from "react-icons/fa";
 const GorevleriGoster = ({todos, setTodos}) => {
   console.log(todos);
 
+  const deleteTodo=(rmv)=>{
+    setTodos(todos.filter((i)=>i.id!==rmv))
+  }
+
 
 
   return (
@@ -12,7 +16,10 @@ const GorevleriGoster = ({todos, setTodos}) => {
           <div className= {x.isDone ? 'done' : 'gorev'}
           onDoubleClick={()=> setTodos(todos.map((a) => a.id===x.id ? {...a, isDone:!a.isDone }:a))}
           >
-            <h3>{x.text} <FaTimesCircle style={{color:'red'}}/> </h3>
+            <h3>{x.text} <FaTimesCircle style={{color:'red'}}
+            onClick={()=>deleteTodo(x.id)}
+
+            /> </h3>
             <h6>{x.day}</h6>
 
 
