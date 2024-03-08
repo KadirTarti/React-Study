1c5af6347ccefd4aadaa78361480f2717516f7af
KadirTarti <tartilaci@gmail.com>
2024-03-08 11:22:59 +0100

    fetch method

--- 08.03.2024-RC-theComponentLifecycle-and-Hooks/src/components/UseEffectAxiosFetch.jsx
+++ 08.03.2024-RC-theComponentLifecycle-and-Hooks/src/components/UseEffectAxiosFetch.jsx
@@ -1,16 +1,26 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 const UseEffectAxiosFetch = () => {
+  const [people, setPeople] = useState([]);
 
 
-  
+  useEffect (()=>{
+    fetch("https://jsonplaceholder.typicode.com/users")
+    .then((res)=>res.json())
+    .then((data)=>setPeople(data));
 
-
-
-
+  }, [])
 
   return (
-    <div>UseEffect-Axios-Fetch</div>
+    <div>
+    {people.map((a)=>{
+      return(
+        <h1>
+        {a.name}
+      </h1>
+      )
+    })}
+    </div>
   )
 }
 
