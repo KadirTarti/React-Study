beff5ad160e2dfd1e622cc8254a6c504cc0c0f45
KadirTarti <tartilaci@gmail.com>
2024-03-10 21:24:53 +0100

    add button

--- TW-15-RandomUserProject/src/App.js
+++ TW-15-RandomUserProject/src/App.js
@@ -1,19 +1,13 @@
 import './App.css';
-//! burada kullanılacak tüm sayfalar üste import edilmek ZORUNDA!
-
 
 import React from 'react';
-import Msg from './msg/Msg';
-import Footer from './footer/Footer';
+import Button from './Button';
 
 function App () {
   return (
-    <div>
-    
-    <Msg/>
-    <Footer/>
-
-
+    <div className='App'>
+    <h1> Random User Generator</h1>
+    <Button/>
     </div>
   )
 }
---
+++ TW-15-RandomUserProject/src/Button.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Button = () => {
+  return (
+    <div>
+    <button>
+    Get User
+    </button>
+    </div>
+  )
+}
+
+export default Button
