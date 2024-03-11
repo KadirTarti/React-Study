73c1cadfd49ef880934d86e329df1f8de7e263d3
KadirTarti <tartilaci@gmail.com>
2024-03-11 09:28:31 +0100

    add api adress

--- 11.03.2024-RC-11-useEffect-database-axios/src/pages/Home.jsx
+++ 11.03.2024-RC-11-useEffect-database-axios/src/pages/Home.jsx
@@ -1,6 +1,13 @@
 import React from 'react'
 
 const Home = () => {
+
+  //* database adresi best practice olarak sadece  burada olur.
+  const url = "https://tutorial-api.fullstack.clarusway.com/tutorials/";
+
+  //! GET (read işlemi -> verileri okuma)
+
+
   return (
     <div>Home</div>
   )
