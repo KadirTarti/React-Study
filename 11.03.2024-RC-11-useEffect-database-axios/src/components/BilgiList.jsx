c9cdf7ce69b21e2867ab12b11059cbba3b45682f
KadirTarti <tartilaci@gmail.com>
2024-03-11 09:57:54 +0100

    add table for data's

--- 11.03.2024-RC-11-useEffect-database-axios/src/components/BilgiList.jsx
+++ 11.03.2024-RC-11-useEffect-database-axios/src/components/BilgiList.jsx
@@ -1,12 +1,42 @@
 import React from 'react'
+import { AiFillDelete } from "react-icons/ai";
+import { FaEdit } from "react-icons/fa";
 
 const BilgiList = ({tutorials}) => {
 
   console.log(tutorials);
 
   return (
-    <div>
-        
+    <div className="container mt-4">
+      <table className="table table-striped">
+        <thead>
+          <tr>
+            <th scope="col">#id</th>
+            <th scope="col">Title</th>
+            <th scope="col">Description</th>
+            <th scope="col" className="text-center">
+              Edit
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          {tutorials.map(({id, title,description}) => {
+            return (
+              <tr key={id}>
+                <th>{id}</th>
+                <td>{title}</td>
+                <td>{description} </td>
+                <td className="text-center ">
+                <AiFillDelete type='button' className='text-danger me-4 fs-5'/>
+                <FaEdit type='button' className='text-warning' size={20}/>
+
+                </td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+   
     </div>
   )
 }
