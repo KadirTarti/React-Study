c374fc6fb51b8a134f9cb3d27c5b4703d370c6ff
KadirTarti <tartilaci@gmail.com>
2024-03-13 10:45:59 +0100

    finish mynavbar

--- 13.03.2024-RC-12-13-react-router/src/App.js
+++ 13.03.2024-RC-12-13-react-router/src/App.js
@@ -5,16 +5,33 @@ import Home from './pages/Home';
 import Teacher from './pages/Teacher';
 import Footer from './components/Footer';
 import CourseCard from './pages/CourseCard';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import ContactForm from './pages/ContactForm';
 
 
 const App = () => {
   return (
     <div>
-    <MyNavbar/>
-    <Home/>
+      <Router>
+        <MyNavbar/>
+          <Routes>
+            <Route path='/' element={<Home/>} />
+            <Route path='/teacher' element={<Teacher/>} />
+            <Route path='/courses' element={<CourseCard/>} />
+            <Route path='/contact' element={<ContactForm/>} />
+            <Route path='/' element={<Home/>} />
+
+
+          </Routes>
+
+        <Footer/>
+    {/* <MyNavbar/>
+    
     <Teacher/>
     <CourseCard/>
-    <Footer/>
+    <Footer/> */}
+      </Router>
+
     </div>
   )
 }
