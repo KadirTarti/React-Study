cfc6410f2fc0808f0fff8efd68bb3025873b0886
KadirTarti <tartilaci@gmail.com>
2024-03-13 09:44:51 +0100

    install aixos

--- 13.03.2024-RC-12-13-react-router/src/App.js
+++ 13.03.2024-RC-12-13-react-router/src/App.js
@@ -1,8 +1,21 @@
 import React from 'react'
 import "bootstrap/dist/css/bootstrap.min.css";
+import MyNavbar from './components/MyNavbar';
+import Home from './pages/Home';
+import Teacher from './pages/Teacher';
+import Footer from './components/Footer';
+import CourseCard from './pages/CourseCard';
+
+
 const App = () => {
   return (
-    <div>App</div>
+    <div>
+    <MyNavbar/>
+    <Home/>
+    <Teacher/>
+    <CourseCard/>
+    <Footer/>
+    </div>
   )
 }
 
