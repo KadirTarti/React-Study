e597922ddf292d84095e2bf34177366d9bd671d8
Abdulxd <tartilaci@gmail.com>
2024-03-15 10:22:39 +0100

    finish teacher and teacher details router

--- 13.03.2024+15.03.2024RC-12-13-react-router/src/App.js
+++ 13.03.2024+15.03.2024RC-12-13-react-router/src/App.js
@@ -9,6 +9,7 @@ import CardDetails from './pages/CardDetails';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import ContactForm from './pages/ContactForm';
 import Login from './pages/Login';
+import TeacherDetails from './pages/TeacherDetails';
 
 
 const App = () => {
@@ -18,7 +19,9 @@ const App = () => {
         <MyNavbar/>
           <Routes>
             <Route path='/' element={<Home/>} />
-            <Route path='/teacher/:ogretmen' element={<Teacher/>} />
+            <Route path='/teacher' element={<Teacher/>} />
+            <Route path='/teacher/:id' element={<TeacherDetails/>} />
+
             <Route path='/courses' element={<CourseCard/>} />
             <Route path='/courses/:nameee' element={<CardDetails/>} />
             <Route path='/contact' element={<ContactForm/>} />
--- 13.03.2024+15.03.2024RC-12-13-react-router/src/pages/Teacher.jsx
+++ 13.03.2024+15.03.2024RC-12-13-react-router/src/pages/Teacher.jsx
@@ -6,11 +6,11 @@ import { useNavigate } from "react-router-dom";
 
 
 const Teacher = () => {
+  const navigate = useNavigate()
+
   const [people, setPeople] = useState([]);
 
-  const naviGate = useNavigate();
-
-  //!4.yol axios async await
+  //!  axios async await
 
   const getData = async () => {
     const res = await axios("https://jsonplaceholder.typicode.com/users");
@@ -23,18 +23,19 @@ const Teacher = () => {
   return (
     <div className="container text-center mt-4">
       <div className="row">
-        {people.map(({ name, username, id, phone }) => {
+        {people.map((kisi) => {
           return (
-            <div key={id} className="col-12 col-sm-6 col-md-4">
+            <div key={kisi.id} className="col-12 col-sm-6 col-md-4">
               {/* https://www.dicebear.com/styles/avataaars/ */}
               <img
-                src={`https://api.dicebear.com/7.x/avataaars/svg?seed=${name}`}
+                src={`https://api.dicebear.com/7.x/avataaars/svg?seed=${kisi.name}`}
                 alt=""
-                onClick={()=>naviGate(`/teacher/${id}`)}
+                // onClick={()=>navigate(`/teacher/${kisi.id}`)}
+                onClick={()=> navigate(`/teacher/${kisi.id}`, {state:{kisi}})}
               />
-              <h5>{name}</h5>
-              <h6>{username} </h6>
-              <h6>{phone}</h6>
+              <h5>{kisi.name}</h5>
+              <h6>{kisi.username} </h6>
+              <h6>{kisi.phone}</h6>
             </div>
           );
         })}
