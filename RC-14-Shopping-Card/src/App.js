1b88aa547838aeeadc23ca6c90ba3b81b42b82c5
Abdulxd <tartilaci@gmail.com>
2024-03-19 09:31:46 +0100

    add routers

--- RC-14-Shopping-Card/src/App.js
+++ RC-14-Shopping-Card/src/App.js
@@ -1,19 +1,21 @@
-import Main from "./pages/Main";
-import NewProduct from "./pages/NewProduct";
-import ProductList from "./pages/ProductList";
-import About from "./pages/About";
-import Navbar from "./components/Navbar";
+// import Main from "./pages/Main";
+// import NewProduct from "./pages/NewProduct";
+// import ProductList from "./pages/ProductList";
+// import About from "./pages/About";
+// import Navbar from "./components/Navbar";
+import AppRouter from "./router/AppRouter";
 
 
 
 function App() {
   return (
     <div>
-      <Navbar/>
+      <AppRouter/>
+      {/* <Navbar/>
       <Main/>
       <About/>
       <NewProduct/>
-      <ProductList/>
+      <ProductList/> */}
      
     </div>
   );
--- RC-14-Shopping-Card/src/router/AppRouter.jsx
+++ RC-14-Shopping-Card/src/router/AppRouter.jsx
@@ -1,16 +1,26 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Main from "./pages/Main";
+import NewProduct from "./pages/NewProduct";
+import ProductList from "./pages/ProductList";
+import About from "./pages/About";
+import UpdateProduct from '../pages/UpdateProduct';
+
 
 const AppRouter = () => {
   return (
-<Navbar/>
-
-
-
-
-
-
-
+    <BrowserRouter>
+    <Navbar/>
+    <Routes>
+      <Route path='/' element={<Main/>} />
+      <Route path='/about' element={<About/>} />
+      <Route path='/new-product' element={<NewProduct/>} />
+      <Route path='/products' element={<ProductList/>} />
+      <Route path='/update-product-' element={<UpdateProduct/>} /> {/* Corrected the missing closing tag */}
+      <Route path='*' element={<Main/>} />
+    </Routes>
+  </BrowserRouter>
   )
 }
 
