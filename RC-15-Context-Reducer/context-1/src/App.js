3a2ccec18b5a767944159c74ef12dc32a070d4b7
Abdulxd <tartilaci@gmail.com>
2024-03-21 09:07:44 +0100

    add files

---
+++ RC-15-Context-Reducer/context-1/src/App.js
@@ -0,0 +1,17 @@
+import {  useState } from "react";
+import data from "./data";
+import Home from "./components/Home";
+
+
+
+const App = () => {
+
+
+  return (
+    <div>
+      <Home />
+    </div>
+  );
+};
+
+export default App;
