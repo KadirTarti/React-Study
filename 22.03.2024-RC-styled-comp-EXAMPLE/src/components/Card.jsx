75685708c20edbb4ba3cd43a7095ca370cb060d5
Abdulxd <tartilaci@gmail.com>
2024-03-22 11:49:39 +0100

    start responsive

--- 22.03.2024-RC-styled-comp-EXAMPLE/src/components/Card.jsx
+++ 22.03.2024-RC-styled-comp-EXAMPLE/src/components/Card.jsx
@@ -1,8 +1,22 @@
+import data from '../data.js'
+import ImageSSS from './styles/ImageSSS';
+import CardStyle from './styles/KartSSS.jsx';
 
 const Card = () => {
   return (
     <div>
-   card
+      {data.map((item)=>(
+        <CardStyle>
+          <div>
+            <h2>{item.title}</h2>
+            <p>{item.body}</p>
+          </div>
+
+          <ImageSSS src={`./images/${item.image}`} alt="" />
+        </CardStyle>
+
+
+      ))}
     </div>
   );
 }
--- 22.03.2024-RC-styled-comp-EXAMPLE/src/components/styles/KartSSS.jsx
+++ 22.03.2024-RC-styled-comp-EXAMPLE/src/components/styles/KartSSS.jsx
@@ -1,3 +1,17 @@
+import styled from 'styled-components';
+
+const CardStyle = styled.div`
+  background-color: lightsalmon;
+  border-radius: 15px;
+  margin-bottom: 2rem;
+  gap: 2rem;
+  padding: 2rem;
+  display: flex;
+  
+
+`;
+
+export default CardStyle;
 
 
 
