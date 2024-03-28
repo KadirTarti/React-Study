e390c17da344be16e41bb89495f6cd64c7947414
Abdulxd <tartilaci@gmail.com>
2024-03-28 15:58:53 +0100

    add button class

--- RC-19-20-movie-app/movie-app-inclass/src/pages/Register.jsx
+++ RC-19-20-movie-app/movie-app-inclass/src/pages/Register.jsx
@@ -14,33 +14,39 @@ const Register = () => {
 
           <div class="relative z-0 w-full mb-5 group">
             <input
-              type="email"
-              name="floating_email"
-              id="floating_email"
-              class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+              type="text"
+              name="firstName"
+              id="firstName"
+              class="peer"
               placeholder=" "
               required
             />
             <label
-              for="floating_email"
-              class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
-            >
-              Email address
-            </label>
+              for="floating_email">First Name</label>
           </div>
-
-          <div>
-            <input name="floating_text" type="text" required />
-            <label htmlFor="floating_text">Last Name</label>
+            
+          <div class="relative z-0 w-full mb-5 group">
+            <input type="text"
+              name="lastName"
+              id="lastName"
+              class="peer"
+              placeholder=" " required />
+            <label htmlFor="lastName">Last Name</label>
           </div>
-          <div>
-            <input name="floating_email" type="email" />
-            <label htmlFor="floating_email">Email</label>
+          <div class="relative z-0 w-full mb-5 group">
+            <input name="email" id='email' type="email" 
+               class="peer" placeholder=""
+            />
+            <label htmlFor="email">Email</label>
           </div>
-          <div>
-            <input name="floating_password" type="password" />
+          <div class="relative z-0 w-full mb-5 group">
+            <input name="password"
+              id="password"
+              class="peer"
+              placeholder=" " type="password" />
             <label htmlFor="floating_password">Password</label>
           </div>
+          
           <button type="submit">Register</button>
           <button type="button">
             Continue with Google
@@ -52,4 +58,4 @@ const Register = () => {
   );
 };
 
-export default Register;
+export default Register                         ;
