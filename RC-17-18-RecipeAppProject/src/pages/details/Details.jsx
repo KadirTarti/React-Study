4b6294ba7b1821f65229a177cca512ba289c76e6
Abdulxd <tartilaci@gmail.com>
2024-03-29 12:02:02 +0100

    update details css

--- RC-17-18-RecipeAppProject/src/pages/details/Details.jsx
+++ RC-17-18-RecipeAppProject/src/pages/details/Details.jsx
@@ -7,7 +7,7 @@ import {
   IngredContainer,
   OtherPart,
 } from "./DetailsStyles";
-import dietsvg from "../../assets/diet.svg";
+import detailsimg from "../../assets/details.webp";
 import { useLocation, useNavigate } from "react-router-dom";
 
 const Details = () => {
@@ -15,61 +15,70 @@ const Details = () => {
     state: { recipe },
   } = useLocation();
 
-  const navigate = useNavigate()
+  const navigate = useNavigate();
 
   return (
     <DetailContainer>
       <HeaderContainer>
         <h1> {recipe.label} </h1>
-        <img style={{cursor:'pointer'}} src={dietsvg} alt="" onClick={()=>navigate(-1)} />
+        <img
+          style={{ cursor: "pointer" }}
+          src={detailsimg}
+          alt=""
+          onClick={() => navigate(-1)}
+        />
       </HeaderContainer>
 
       <DetailPart>
-
-      <ImgContainer>
+        <ImgContainer>
           <img src={recipe.image} alt="" />
         </ImgContainer>
 
-
         <OtherPart>
-          <>Nutrients</>
+          <div>
+          <p>
+            {recipe.totalNutrients.CA.label}:&nbsp;
+            {Math.round(recipe.totalNutrients.CA.quantity)}
+            {recipe.totalNutrients.CA.unit}{" "}
+          </p>
 
-          <p>{recipe.totalNutrients.CA.label}:&nbsp;  
-          {Math.round(recipe.totalNutrients.CA.quantity)}
-          {recipe.totalNutrients.CA.unit}           </p>
-          
-          <p>{recipe.totalNutrients.CHOCDF.label}:&nbsp;  
-          {Math.round(recipe.totalNutrients.CHOCDF.quantity)}
-          {recipe.totalNutrients.CHOCDF.unit} </p>
-          
-          <p>{recipe.totalNutrients.CHOLE.label}:&nbsp;  
-          {Math.round(recipe.totalNutrients.CHOLE.quantity)}
-          {recipe.totalNutrients.CHOLE.unit} </p>
+          <p>
+            {recipe.totalNutrients.CHOCDF.label}:&nbsp;
+            {Math.round(recipe.totalNutrients.CHOCDF.quantity)}
+            {recipe.totalNutrients.CHOCDF.unit}{" "}
+          </p>
 
-          <p>{recipe.totalNutrients.ENERC_KCAL.label}:&nbsp;  
-          {Math.round(recipe.totalNutrients.ENERC_KCAL.quantity)}
-          {recipe.totalNutrients.ENERC_KCAL.unit} </p>
+          <p>
+            {recipe.totalNutrients.CHOLE.label}:&nbsp;
+            {Math.round(recipe.totalNutrients.CHOLE.quantity)}
+            {recipe.totalNutrients.CHOLE.unit}{" "}
+          </p>
+
+          <p>
+            {recipe.totalNutrients.ENERC_KCAL.label}:&nbsp;
+            {Math.round(recipe.totalNutrients.ENERC_KCAL.quantity)}
+            {recipe.totalNutrients.ENERC_KCAL.unit}{" "}
+          </p>
 
           <p>{Math.round(recipe.totalWeight)} </p>
           <p>Calories: {Math.round(recipe.calories)} </p>
           {/* digest key i ile bize 29 elemanlı bir dizi geliyor, biz onun 4 elemanını bastırmak için slice kullandık */}
           <p>
-            {recipe.digest.slice(0,4).map((item, index)=>(
-              <p  key={index} style={{color:'white'}}>{item.label} : {Math.round(item.total)}</p>
-
+            {recipe.digest.slice(0, 4).map((item, index) => (
+              <p key={index} style={{ color: "darkblue", fontWeight:'400', fontStyle:'italic' }}>
+                {item.label} : {Math.round(item.total)}
+              </p>
             ))}
           </p>
+          </div>
         </OtherPart>
 
-       
-
         <IngredContainer>
-          {recipe.ingredientLines.map((item, index)=>(
+          {recipe.ingredientLines.map((item, index) => (
             <div>
-            <p>
-              {index+1}-) {item}
-            </p>
-
+              <p>
+                {index + 1}-) {item}
+              </p>
             </div>
           ))}
         </IngredContainer>
--- RC-17-18-RecipeAppProject/src/pages/details/DetailsStyles.jsx
+++ RC-17-18-RecipeAppProject/src/pages/details/DetailsStyles.jsx
@@ -6,7 +6,7 @@ export const DetailContainer = styled.div`
   flex-wrap: wrap;
   flex-direction: column;
   justify-content: center;
-  background: #00adb5;
+  background: #3A0088;
   min-height: calc(100vh - 75px);
   /* height: fit-content; */
   padding: 5px;
@@ -19,26 +19,21 @@ export const DetailPart = styled.div`
   justify-content: center;
   flex-wrap: wrap;
   align-items: center;
-  margin: 1rem;
-  border: 1px solid white;
-  border-radius: 5px;
-  background-color: pink;
+  margin: 1.5rem;
+  border-radius: 15px;
+  background-color: #7da3bb;;;
   /* background-color: #e1f1dd; */
   /* height: calc(100vh - 300px); */
   @media (max-width: 1250px) {
     justify-content: center;
-
-    border: 1px solid red;
   }
 `;
 
 export const ImgContainer = styled.div`
   /* resmin div i */
-  background-color: green;
-
-  border: 1px solid #00adb5;
-  padding: 10px;
-  border-radius: 3px;
+  background-color: #930077;
+  padding: 6px;
+  border-radius: 10px;
   margin: 2rem;
   img {
     border-radius: 10px;
@@ -50,34 +45,51 @@ export const ImgContainer = styled.div`
 
 export const HeaderContainer = styled.div`
   /* üstteki başlık ve kadın resmi */
-  background-color: darkgray;
-
+  color:  gold;
+  text-shadow: -1px -1px 5px pink;
   display: flex;
-  justify-content: space-evenly;
+  justify-content: center;
+  gap: 2rem;
   margin: 25px;
-  font-size: 3rem;
   h1 {
     font-size: 3rem;
   }
   img {
-    width: 200px;
+    width: 150px;
+    height: 140px;
   }
 `;
 
 export const IngredContainer = styled.div`
   /* alttaki bilgiler, içindekiler */
-  width: 400px;
+  width: 300px;
   font-size: 1.4rem;
-  margin: 2rem;
-  background-color: gray;
+  font-weight: 900;
+  padding:  5px 15px;
+  margin: 1rem;
+  background-color: gold;
+  border: 5px solid #930077;
+  border-radius: 10px;
+  color: #620050;
 `;
 
 export const OtherPart = styled.div`
   /* üstteki bilgiler */
-  background-color: lightgray;
+  display: flex;
+  flex-direction: column;
+  line-height: 15px;
+  background-color: rgb(255, 235, 202);
+  border-radius: 10px;
+  color:#3A0088;
+  font-weight: 600;
+  border: 5px solid #3A0088;
 
   width: 400px;
   text-align: right;
-  font-size: 1.8rem;
-  margin: 2rem;
+  font-size: 1.3rem;
+  margin: 1rem;
+
+  div{
+    text-align: center;
+  }
 `;
