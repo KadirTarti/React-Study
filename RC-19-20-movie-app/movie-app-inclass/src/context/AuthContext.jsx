187b62043c88585246cfbf8cf9e2dd268c8c9ac3
Abdulxd <tartilaci@gmail.com>
2024-04-02 11:18:16 +0200

    add keys envlocal

--- RC-19-20-movie-app/movie-app-inclass/src/context/AuthContext.jsx
+++ RC-19-20-movie-app/movie-app-inclass/src/context/AuthContext.jsx
@@ -1,23 +1,19 @@
 import { createContext, useContext, useState } from "react";
 
 //! create context
-
 const AuthContext = createContext();
 
 //? context provider
 
 const AuthContextProvider = ({children}) => {
-    
-    const [currentUser, setCurrentUser] = useState('')
-
+    const [currentUser,setCurrentUser] = useState("")
 
     return <AuthContext.Provider value={{currentUser}}>{children}</AuthContext.Provider>
 };
 
-//* consume with costum hook
-
+//* consumer with custom hook
 export const useAuthContext = () => {
     return useContext(AuthContext)
 }
 
-export default AuthContextProvider
+export default AuthContextProvider;
