18f6769e9f7adcfd028b52150d0fe2ff490b3b4d
Abdulxd <tartilaci@gmail.com>
2024-04-03 09:34:47 +0200

    add signGoogleProvider

--- RC-19-20-movie-app/movie-app-inclass/src/context/AuthContext.jsx
+++ RC-19-20-movie-app/movie-app-inclass/src/context/AuthContext.jsx
@@ -1,7 +1,15 @@
-import { createUserWithEmailAndPassword, onAuthStateChanged, signInWithEmailAndPassword, signOut, updateProfile } from "firebase/auth";
+import {
+  GoogleAuthProvider,
+  createUserWithEmailAndPassword,
+  onAuthStateChanged,
+  signInWithEmailAndPassword,
+  signInWithPopup,
+  signOut,
+  updateProfile,
+} from "firebase/auth";
 import { createContext, useContext, useEffect, useState } from "react";
 import { auth } from "../auth/firebase";
-import {useNavigate} from "react-router-dom"
+import { useNavigate } from "react-router-dom";
 
 //! create context
 const AuthContext = createContext();
@@ -10,10 +18,10 @@ const AuthContext = createContext();
 
 const AuthContextProvider = ({ children }) => {
   const [currentUser, setCurrentUser] = useState("");
-  const navigate = useNavigate()
+  const navigate = useNavigate();
 
-  const register = async (email, password,displayName) => {
-        //? yeni bir kullanıcı oluşturmak için kullanılan firebase metodu
+  const register = async (email, password, displayName) => {
+    //? yeni bir kullanıcı oluşturmak için kullanılan firebase metodu
     const userCredential = await createUserWithEmailAndPassword(
       auth,
       email,
@@ -21,48 +29,58 @@ const AuthContextProvider = ({ children }) => {
     );
     //? kullanıcı profilini güncellemek için kullanılan firebase metodu
     await updateProfile(auth.currentUser, {
-        displayName: displayName
-      })
-      navigate("/")
+      displayName: displayName,
+    });
+    navigate("/");
     console.log(userCredential);
   };
-     //* https://console.firebase.google.com/
+  //* https://console.firebase.google.com/
   //* => Authentication => sign-in-method => enable Email/password
   //! Email/password ile girişi enable yap
   const login = async (email, password) => {
-     await signInWithEmailAndPassword(auth, email, password)
-     navigate("/")
+    await signInWithEmailAndPassword(auth, email, password);
+    navigate("/");
   };
   const logout = () => {
     //*https://firebase.google.com/docs/auth/web/password-auth#next_steps
-    signOut(auth)//! sadece signOut metodunu çağırmamız yeterli
-  }
+    signOut(auth); //! sadece signOut metodunu çağırmamız yeterli
+  };
+
+  const signGoogleProvider = async () => {
+    try {
+      const provider = new GoogleAuthProvider();
+      await signInWithPopup(auth, provider);
+      navigate("/");
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   const userObserver = () => {
     //? Kullanıcının signin olup olmadığını takip eden ve kullanıcı değiştiğinde yeni kullanıcıyı response olarak dönen firebase metodu
     onAuthStateChanged(auth, (user) => {
-        if (user) {
-          // User is signed in, see docs for a list of available properties
-          // https://firebase.google.com/docs/reference/js/auth.user
-          console.log(user)
-          const {email,displayName,photoURL} = user;
-          console.log(user.displayName)
-          setCurrentUser({email,displayName,photoURL})
-        } else {
-          // User is signed out
-          // ...
-          setCurrentUser(false)
-        }
-      });
-  }
+      if (user) {
+        // User is signed in, see docs for a list of available properties
+        // https://firebase.google.com/docs/reference/js/auth.user
+        console.log(user);
+        const { email, displayName, photoURL } = user;
+        console.log(user.displayName);
+        setCurrentUser({ email, displayName, photoURL });
+      } else {
+        // User is signed out
+        // ...
+        setCurrentUser(false);
+      }
+    });
+  };
 
-  console.log(currentUser)
-  useEffect(()=>{
-    userObserver()//* Kullanıcı giriş çıkışlarını takip ettirmesi için userObserverı tetikliyoruz
-  },[])
+  console.log(currentUser);
+  useEffect(() => {
+    userObserver(); //* Kullanıcı giriş çıkışlarını takip ettirmesi için userObserverı tetikliyoruz
+  }, []);
 
   return (
-    <AuthContext.Provider value={{ currentUser,register,login,logout }}>
+    <AuthContext.Provider value={{ currentUser, register, login, logout, signGoogleProvider }}>
       {children}
     </AuthContext.Provider>
   );
--- RC-19-20-movie-app/movie-app-inclass/src/router/AppRouter.jsx
+++ RC-19-20-movie-app/movie-app-inclass/src/router/AppRouter.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter, Route, Router, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import Main from "../pages/Main";
 import Register from "../pages/Register";
 import Login from "../pages/Login";
