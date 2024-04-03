478ae4fc19b3bc161aa5ed2fdf763f688e61cbb6
Abdulxd <tartilaci@gmail.com>
2024-04-03 11:05:21 +0200

    add MovieContext

--- RC-19-20-movie-app/movie-app-inclass/src/App.js
+++ RC-19-20-movie-app/movie-app-inclass/src/App.js
@@ -1,15 +1,18 @@
 import React from "react";
 import AppRouter from "./router/AppRouter";
 import AuthContextProvider from "./context/AuthContext";
-import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import MovieContextProvider from "./context/MovieContext";
 
 const App = () => {
   return (
     <div>
-    <AuthContextProvider>
-      <AppRouter/>
-      <ToastContainer/>
+      <AuthContextProvider>
+        <MovieContextProvider>
+          <AppRouter />
+          <ToastContainer />
+        </MovieContextProvider>
       </AuthContextProvider>
     </div>
   );
