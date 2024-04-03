9f00e03e32108a2ea7543d9ee18c40f5f238385f
Abdulxd <tartilaci@gmail.com>
2024-04-03 12:47:10 +0200

    update searchInput

--- RC-19-20-movie-app/movie-app-inclass/src/context/SearchInput.jsx
+++ RC-19-20-movie-app/movie-app-inclass/src/context/SearchInput.jsx
@@ -1,42 +1,36 @@
 import React, { useState } from 'react'
-import { useAuthContext } from '../context/AuthContext';
+import { useAuthContext } from '../context/AuthContext'
 import { useMovieContext } from '../context/MovieContext';
 import { toastWarnNotify } from '../helpers/toastNotify';
 
 const SearchInput = () => {
-
-    const [searchTerm, setSearchTerm] = useState(''); 
+    const [searchTerm,setSearchTerm] = useState("")
     const {currentUser} = useAuthContext();
-    const {getMovies} = useMovieContext();
+    const {getMovies} = useMovieContext()
 
     const searchUrl = `https://api.themoviedb.org/3/search/movie?api_key=${process.env.REACT_APP_apiKey}&query=`
 
-    const handleSubmit = (e) =>{
+    const handleSubmit = (e) => {
         e.preventDefault();
         if(currentUser && searchTerm.trim()){
             getMovies(searchUrl+searchTerm)
-        } else if (!currentUser) {
-            toastWarnNotify('Please log in!')
-        } else {
-            toastWarnNotify('Please enter text!')
+        }else if (!currentUser) {
+            toastWarnNotify("Please log in")
+        }else {
+            toastWarnNotify("Please enter text!")
         }
     }
-
-
   return (
-
     <form onSubmit={handleSubmit}  className="flex justify-center p-2">
         <input
           type="search"
           name="query"
           className="w-80 h-8 rounded-md p-1 m-2"
           placeholder="Search a movie..."
-          onChange={(e)=> setSearchTerm(e.target.value)}
+          onChange={(e)=>setSearchTerm(e.target.value)}
         />
         <button className="btn-danger-bordered">Search</button>
       </form>
-
-
   )
 }
 
