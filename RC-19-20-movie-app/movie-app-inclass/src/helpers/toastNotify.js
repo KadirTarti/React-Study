e17e558e2c1ed530299d4f271982f579b3c61659
Abdulxd <tartilaci@gmail.com>
2024-04-03 10:14:44 +0200

    update toastnotify

--- RC-19-20-movie-app/movie-app-inclass/src/helpers/toastNotify.js
+++ RC-19-20-movie-app/movie-app-inclass/src/helpers/toastNotify.js
@@ -1,7 +1,7 @@
-import { toast } from 'react-toastify';
+import { toast } from "react-toastify";
 
-export const toastSuccessNotify = (msg) => 
-toast.success(msg, {
+export const toastSuccessNotify = (msg) =>
+  toast.success(msg, {
     position: "top-right",
     autoClose: 5000,
     hideProgressBar: false,
@@ -10,4 +10,25 @@ toast.success(msg, {
     draggable: true,
     progress: undefined,
     theme: "colored",
+  });
+
+  export const toastWarnNotify = (msg) => {
+    toast.warn(msg, {
+      autoClose: 5000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
     });
+  };
+  export const toastErrorNotify = (msg) => {
+    toast.error(msg, {
+      autoClose: 5000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+    });
+  };
