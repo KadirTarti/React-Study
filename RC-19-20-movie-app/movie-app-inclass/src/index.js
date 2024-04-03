3f25d3a7c68bbeabbd7bb127341822814dc31aeb
Abdulxd <tartilaci@gmail.com>
2024-04-03 09:02:49 +0200

    update indexjs

--- RC-19-20-movie-app/movie-app-inclass/src/index.js
+++ RC-19-20-movie-app/movie-app-inclass/src/index.js
@@ -1,9 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./App";
-import "react-toastify/dist/ReactToastify.css";
 import { BrowserRouter } from "react-router-dom";
+import "react-toastify/dist/ReactToastify.css";
+import App from "./App";
+import "./index.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
