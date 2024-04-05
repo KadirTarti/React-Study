e618a397b29d7fe48a6b84e2d4a5316d414e4643
tarti <tartilaci@gmail.com>
2024-04-05 11:12:21 +0200

    add examples

--- 05.04.2024-inClass-Vite-PNPM/mui-intro/src/pages/Home.jsx
+++ 05.04.2024-inClass-Vite-PNPM/mui-intro/src/pages/Home.jsx
@@ -1,9 +1,40 @@
-import React from 'react'
+import React from "react";
+import Container from "@mui/material/Container";
+import Typography from "@mui/material/Typography";
 
 const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <Container
+      style={{ background: "gray", height: "100vh", maxWidth: "500px" }}
+    >
+      <Typography variant="h3" component="h1" align="center" color="primary">
+        h1. Heading
+      </Typography>
 
-export default Home
+      <Typography
+        variant="h3"
+        component="h1"
+        align="center"
+        color="secondary"
+        mt={5}
+      >
+        Mui Elements
+      </Typography>
+
+      <Typography
+        variant="body1"
+        align="justify"
+        mt={5}
+        sx={{ bgcolor: "red", border: "3px solid blue", color: "white" }}
+      >
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium
+        culpa consequatur cum voluptatem ipsum illum impedit possimus quam nihil
+        saepe! Rem itaque deserunt odio repudiandae nulla. Praesentium non
+        commodi tenetur cum porro exercitationem temporibus obcaecati laboriosam
+        sed. Assumenda, in debitis.
+      </Typography>
+    </Container>
+  );
+};
+
+export default Home;
