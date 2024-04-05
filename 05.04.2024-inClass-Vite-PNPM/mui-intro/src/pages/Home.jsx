41ffc86d9f148d7910eab4684a1ba13b3fa15a4f
tarti <tartilaci@gmail.com>
2024-04-05 11:27:55 +0200

    add box and stack props

--- 05.04.2024-inClass-Vite-PNPM/mui-intro/src/pages/Home.jsx
+++ 05.04.2024-inClass-Vite-PNPM/mui-intro/src/pages/Home.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import Container from "@mui/material/Container";
 import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+import Box from "@mui/material/Box";
+import Stack from "@mui/material/Stack";
 
 const Home = () => {
   return (
     <Container
-      style={{ background: "gray", height: "100vh", maxWidth: "500px" }}
+      style={{ background: "wheat", height: "100vh", maxWidth: "1500px" }}
     >
       <Typography variant="h3" component="h1" align="center" color="primary">
         h1. Heading
@@ -33,6 +36,32 @@ const Home = () => {
         commodi tenetur cum porro exercitationem temporibus obcaecati laboriosam
         sed. Assumenda, in debitis.
       </Typography>
+
+      <Box 
+      display='flex'
+      justifyContent='center'
+      alignItems='center'
+      gap={3}
+      m={7}
+      >
+        <Button variant="text">Text</Button>
+        <Button variant="contained">Contained</Button>
+        <Button variant="outlined">Outlined</Button>
+      </Box>
+
+      import Stack from '@mui/material/Stack';
+
+ <Stack
+        direction={{ xs: 'column', sm: 'row' }}
+        spacing={{ xs: 1, sm: 2, md: 4 }}
+        gap={3}
+        m={5}
+      >
+      <Button variant="text">Text</Button>
+        <Button variant="contained" color='error'>Contained</Button>
+        <Button variant="outlined">Outlined</Button>
+
+        </Stack>
     </Container>
   );
 };
