66cc854d0c279dc460174d8740846a3523212c02
tarti <tartilaci@gmail.com>
2024-04-15 12:47:30 +0200

    add navigate to useAuthCall

--- MY_Stock-APP_RC-24-29/MY_inclass-formik-yup/src/hook/useAuthCall.jsx
+++ MY_Stock-APP_RC-24-29/MY_inclass-formik-yup/src/hook/useAuthCall.jsx
@@ -1,16 +1,20 @@
 import React from 'react'
 import axios from 'axios'
 import { useDispatch } from 'react-redux'
-import { fetchFail, fetchStart } from '../features/authSlice'
+import { useNavigate } from 'react-router-dom'
+import { fetchFail, fetchStart, registerSuccess } from '../features/authSlice'
 
 const useAuthCall = () => {
   const dispatch = useDispatch()
+  const navigate = useNavigate()
 
   const register = async (userInfo) => {
     dispatch(fetchStart())
     try {
         const {data} = await axios.post("https://10002.fullstack.clarusway.com/users/",userInfo)
-        console.log(data)
+        // console.log(data)
+        dispatch (registerSuccess(data))
+        navigate("/stock")
     } catch (error) {
         dispatch(fetchFail())
         console.log(error)
--- MY_Stock-APP_RC-24-29/MY_inclass-formik-yup/src/router/PrivateRouter.jsx
+++ MY_Stock-APP_RC-24-29/MY_inclass-formik-yup/src/router/PrivateRouter.jsx
@@ -1,8 +1,11 @@
 import { Navigate, Outlet } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 const PrivateRouter = () => {
 
-  return true ? <Outlet /> : <Navigate to="/" />;
+  const {currentUser} = useSelector(state=> state.auth)
+
+  return currentUser ? <Outlet /> : <Navigate to="/" />;
 };
 
 export default PrivateRouter;
