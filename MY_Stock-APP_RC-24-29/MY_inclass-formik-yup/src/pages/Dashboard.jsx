c8e9e8ec968493279db31bee51c6afab078e0435
tarti <tartilaci@gmail.com>
2024-04-17 08:28:10 +0200

    update dashboard

--- MY_Stock-APP_RC-24-29/MY_inclass-formik-yup/src/pages/Dashboard.jsx
+++ MY_Stock-APP_RC-24-29/MY_inclass-formik-yup/src/pages/Dashboard.jsx
@@ -5,9 +5,10 @@ import CssBaseline from "@mui/material/CssBaseline";
 import Toolbar from "@mui/material/Toolbar";
 import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
+import useAuthCall from "../hooks/useAuthCall";
 
 function Dashboard() {
-
+  const {logout} = useAuthCall()
   return (
     <Box sx={{ display: "flex" }}>
       <CssBaseline />
@@ -16,7 +17,7 @@ function Dashboard() {
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
             STOCK APP
           </Typography>
-          <Button color="inherit">Logout</Button>
+          <Button color="inherit" onClick={logout}>Logout</Button>
         </Toolbar>
       </AppBar>
     </Box>
