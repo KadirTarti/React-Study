468156c984ca7d54053e22b40ceca396605c69ef
tarti <tartilaci@gmail.com>
2024-04-17 08:19:35 +0200

    add registerform

--- MY_Stock-APP_RC-24-29/MY_inclass-formik-yup/src/pages/Register.jsx
+++ MY_Stock-APP_RC-24-29/MY_inclass-formik-yup/src/pages/Register.jsx
@@ -1,37 +1,16 @@
-import LockIcon from "@mui/icons-material/Lock";
-import { Box, Button, TextField } from "@mui/material";
-import Avatar from "@mui/material/Avatar";
 import Container from "@mui/material/Container";
-import Grid from "@mui/material/Grid";
 import Typography from "@mui/material/Typography";
-import { Form, Formik } from "formik";
-import { Link } from "react-router-dom";
-import * as Yup from "yup";
+import Avatar from "@mui/material/Avatar";
+import LockIcon from "@mui/icons-material/Lock";
 import image from "../assets/regi.avif";
+import Grid from "@mui/material/Grid";
+import { Link } from "react-router-dom";
+import { Box } from "@mui/material";
 import AuthHeader from "../components/AuthHeader";
 import AuthImage from "../components/AuthImage";
-import useAuthCall from "../hook/useAuthCall";
-
-const SignupSchema = Yup.object().shape({
-  username: Yup.string().min(3).max(15).required("Required!"),
-  firstName: Yup.string()
-    .min(2, "Too Short!")
-    .max(50, "Too Long!")
-    .required("Required"),
-  lastName: Yup.string()
-    .min(2, "Too Short!")
-    .max(50, "Too Long!")
-    .required("Required"),
-  email: Yup.string().email("Invalid email").required("Required"),
-  password: Yup.string()
-  .min(8, 'Er muss mindestens 8 Zeichen lang sein')
-  .max(40, 'Er darf maximal 40 Zeichen lang sein')
-  .required()
-  .matches(/\d+/,'Muss mindestends eine Ziffer enthalten!',)
-  .matches(/[A-Z]/, 'Muss mindestends einen Großbuchstaben enthalten')
-  .matches(/[a-z]/, "Muss mindestens einen Kleinbuchstaben enthalten!")
-  .matches(/[@$?!.,%&*+-]/, 'Muss mindestens en Sonderzeichen enthalten (@$?!.,%&*+-)')
-});
+import { Formik} from "formik";
+import useAuthCall from "../hooks/useAuthCall";
+import RegisterForm, { SignupSchema } from "../components/RegisterForm";
 
 const Register = () => {
   const {register} = useAuthCall()
@@ -72,89 +51,19 @@ const Register = () => {
           <Formik
             initialValues={{
               username: "",
-              password: "",
-              email: "",
               firstName: "",
               lastName: "",
+              email: "",
+              password: "",
             }}
             validationSchema={SignupSchema}
-            onSubmit={(values) => {
-              // same shape as initial values
-              console.log(values);
-              register(values)
+            onSubmit={(values, actions) => {
+              register(values);
+              actions.resetForm();
+              actions.setSubmitting(false);
             }}
-          >
-            {({
-              values,
-              errors,
-              touched,
-              handleChange,
-              handleBlur,
-              handleSubmit,
-              isSubmitting,
-              /* and other goodies */
-            }) => (
-              <Form>
-                <Box sx={{ display:"flex",flexDirection:"column",gap:2}}>
-                  <TextField
-                    id="username"
-                    name="username"
-                    label="Username"
-                    value={values.username}
-                    onChange={handleChange}
-                    onBlur={handleBlur}
-                    error={touched.username && Boolean(errors.username)}
-                    helperText={touched.username && errors.username}
-                  />
-                  <TextField
-                    id="firstName"
-                    name="firstName"
-                    label="FirstName"
-                    value={values.firstName}
-                    onChange={handleChange}
-                    onBlur={handleBlur}
-                    error={touched.firstName && Boolean(errors.firstName)}
-                    helperText={touched.firstName && errors.firstName}
-                  />
-                  <TextField
-                    id="lastName"
-                    name="lastName"
-                    label="LastName"
-                    value={values.lastName}
-                    onChange={handleChange}
-                    onBlur={handleBlur}
-                    error={touched.lastName && Boolean(errors.lastName)}
-                    helperText={touched.lastName && errors.lastName}
-                  />
-
-                  <TextField
-                    id="email"
-                    name="email"
-                    label="Email"
-                    type="email"
-                    value={values.email}
-                    onChange={handleChange}
-                    onBlur={handleBlur}
-                    error={touched.email && Boolean(errors.email)}
-                    helperText={touched.email && errors.email}
-                  />
-                  <TextField
-                    id="password"
-                    name="password"
-                    type="password"
-                    label="Password"
-                    value={values.password}
-                    onChange={handleChange}
-                    onBlur={handleBlur}
-                    error={touched.password && Boolean(errors.password)}
-                    helperText={touched.password && errors.password}
-                  />
-                  <Button variant="contained" type="submit">Sign Up</Button>
-                </Box>
-              </Form>
-            )}
-          </Formik>
-
+            component={(props) => <RegisterForm {...props} />}
+          ></Formik>
           <Box sx={{ textAlign: "center", mt: 2, color: "secondary.main" }}>
             <Link to="/">Already have an account? Sign in</Link>
           </Box>
