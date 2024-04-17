33b87ef7c286093ee6d92b1362d46d064c4311b3
tarti <tartilaci@gmail.com>
2024-04-17 08:28:37 +0200

    update login

--- MY_Stock-APP_RC-24-29/MY_inclass-formik-yup/src/pages/Login.jsx
+++ MY_Stock-APP_RC-24-29/MY_inclass-formik-yup/src/pages/Login.jsx
@@ -1,6 +1,5 @@
 import Avatar from "@mui/material/Avatar";
 import Box from "@mui/material/Box";
-import { useTheme } from "@mui/material";
 import Container from "@mui/material/Container";
 import Grid from "@mui/material/Grid";
 import Typography from "@mui/material/Typography";
@@ -9,10 +8,12 @@ import image from "../assets/hero.png";
 import { Link } from "react-router-dom";
 import AuthHeader from "../components/AuthHeader";
 import AuthImage from "../components/AuthImage";
+import { Formik } from "formik";
+import useAuthCall from "../hooks/useAuthCall";
+import LoginForm, { loginScheme } from "../components/LoginForm";
 
 const Login = () => {
-  const theme = useTheme();
-
+  const { login } = useAuthCall();
   return (
     <Container maxWidth="lg">
       <Grid
@@ -41,6 +42,17 @@ const Login = () => {
             SIGN IN
           </Typography>
 
+          <Formik
+            initialValues={{ email: "", password: "" }}
+            validationSchema={loginScheme}
+            onSubmit={(values, actions) => {
+              login(values);
+              actions.resetForm();
+              actions.setSubmitting(false);
+            }}
+            component={props => <LoginForm {...props} />}>
+              
+            </Formik>
           <Box sx={{ textAlign: "center", mt: 2, color: "secondary.main" }}>
             <Link to="/register">
               Don't have an account? Sign Up
