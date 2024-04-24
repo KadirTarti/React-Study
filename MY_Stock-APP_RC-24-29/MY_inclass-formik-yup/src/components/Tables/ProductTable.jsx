dca9f499c2640b15d83f2a7443e4931591082fbf
tarti <tartilaci@gmail.com>
2024-04-24 10:51:34 +0200

    Update ProductTable component: - Change id field to \_id in columns - Replace sample data (firstName, lastName, age) with actual product data (categoryId, brandId, name, quantity) - Comment out

--- MY_Stock-APP_RC-24-29/MY_inclass-formik-yup/src/components/Tables/ProductTable.jsx
+++ MY_Stock-APP_RC-24-29/MY_inclass-formik-yup/src/components/Tables/ProductTable.jsx
@@ -4,29 +4,36 @@ import { DataGrid } from '@mui/x-data-grid';
 import { useSelector } from 'react-redux';
 
 const columns = [
-  { field: 'id', headerName: 'ID', width: 90 },
+  { field: '_id', headerName: 'ID', width: 90 },
   {
-    field: 'firstName',
-    headerName: 'First name',
+    field: 'categoryId',
+    headerName: 'Category',
     width: 150,
     editable: true,
   },
   {
-    field: 'lastName',
-    headerName: 'Last name',
+    field: 'brandId',
+    headerName: 'Brand',
     width: 150,
     editable: true,
   },
   {
-    field: 'age',
-    headerName: 'Age',
+    field: 'name',
+    headerName: 'Name',
     type: 'number',
     width: 110,
     editable: true,
   },
   {
-    field: 'fullName',
-    headerName: 'Full name',
+    field: 'quantity',
+    headerName: 'Stock',
+    type: 'number',
+    width: 110,
+    editable: true,
+  },
+  {
+    field: 'actions',
+    headerName: 'Actions',
     description: 'This column has a value getter and is not sortable.',
     sortable: false,
     width: 160,
@@ -34,17 +41,17 @@ const columns = [
   },
 ];
 
-const rows = [
-  { id: 1, lastName: 'Snow', firstName: 'Jon', age: 14 },
-  { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 31 },
-  { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 31 },
-  { id: 4, lastName: 'Stark', firstName: 'Arya', age: 11 },
-  { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
-  { id: 6, lastName: 'Melisandre', firstName: null, age: 150 },
-  { id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
-  { id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
-  { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
-];
+// const rows = [
+//   { id: 1, lastName: 'Snow', firstName: 'Jon', age: 14 },
+//   { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 31 },
+//   { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 31 },
+//   { id: 4, lastName: 'Stark', firstName: 'Arya', age: 11 },
+//   { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
+//   { id: 6, lastName: 'Melisandre', firstName: null, age: 150 },
+//   { id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
+//   { id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
+//   { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
+// ];
 
 function getRowId(row) {
     console.log(row);
