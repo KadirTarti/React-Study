e718bbd451f899378e9103cc245fcffa3b1cb1bc
tarti <tartilaci@gmail.com>
2024-04-24 23:37:54 +0200

    "Add purchases page with list of purchases and ability to add new purchase"
    This diff adds the Purchases page which displays a list of purchases and allows the user to add a new purchase with a modal. The page uses Material UI components

--- MY_Stock-APP_RC-24-29/MY_inclass-formik-yup/src/pages/Purchases.jsx
+++ MY_Stock-APP_RC-24-29/MY_inclass-formik-yup/src/pages/Purchases.jsx
@@ -1,16 +1,68 @@
-import React from 'react'
-import useStockCall from '../hooks/useStockCall';
+import React, { useEffect } from "react";
+import Button from "@mui/material/Button";
+import Container from "@mui/material/Container";
+import Grid from "@mui/material/Grid";
+import Typography from "@mui/material/Typography";
+import { useSelector } from "react-redux";
+import useStockCall from "../hooks/useStockCall";
+import PurchasesModal from "../components/Modals/PurchasesModal";
+import { useState } from "react";
 
 const Purchases = () => {
-  const {getStockData} = useStockCall();
 
-  useEffect(() => {
-    getStockData("firms");
-  }, []);
- 
-  return (
-    <div>Purchases</div>
-  )
+
+  const {
+    getStockData,
+  } = useStockCall();
+  const { Purchases } = useSelector((state) => state.stock);
+  const [open, setOpen] = useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => {
+    setOpen(false);
+  setInitialState({
+    name:'',
+    phone:'',
+    address:'',
+    image:''
+  })
 }
 
-export default Purchases
+  const [initialState, setInitialState] = useState({
+    name:'',
+    phone:'',
+    address:'',
+    image:''
+  });
+  console.log("purchases:", Purchases);
+  console.log('purchases', initialState);
+
+
+  useEffect(() => {
+    // getPurchases()
+    getStockData("purchases");
+  }, []);
+
+  return (
+    <Container>
+      <Typography
+        align="center"
+        variant="h4"
+        component="h1"
+        color="secondary.second"
+      >
+        Purchases
+      </Typography>
+      <Button variant="contained" onClick={handleOpen}>New Firm</Button>
+      <Grid container spacing={2} mt={3}>
+        {Purchases.map((firm) => (
+          <Grid item xs={12} md={6} lg={4} xl={3} key={firm._id}>
+            <FirmCard {...firm} handleOpen={handleOpen} setInitialState={setInitialState}/>
+          </Grid>
+        ))}
+      </Grid>
+      {open && <FirmModal open={open} handleClose={handleClose} initialState={initialState}/>}
+    </Container>
+  );
+}
+
+export default Purchases;
