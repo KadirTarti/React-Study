08819a2fdfdf5738b30e67d66a3fce8a56ed8a4c
tarti <tartilaci@gmail.com>
2024-04-24 10:26:30 +0200

    download x-data grid / add ProductTable

--- MY_Stock-APP_RC-24-29/MY_inclass-formik-yup/src/components/Modals/ProductModal.jsx
+++ MY_Stock-APP_RC-24-29/MY_inclass-formik-yup/src/components/Modals/ProductModal.jsx
@@ -4,7 +4,7 @@ import Button from "@mui/material/Button";
 import Modal from "@mui/material/Modal";
 import * as React from "react";
 import useStockCall from "../../hooks/useStockCall";
-import { flexColumn, modalStyle } from "../../styles/globalStyle";
+import { flexColumn, modalStyle } from "../../styles/globalStyle"
 import InputLabel from '@mui/material/InputLabel';
 import MenuItem from '@mui/material/MenuItem';
 import FormControl from '@mui/material/FormControl';
