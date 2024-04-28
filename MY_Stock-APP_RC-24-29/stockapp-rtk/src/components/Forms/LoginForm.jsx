afc4d80d0b1eb4652c6a412a9cf649a5a45ba3a4
tarti <tartilaci@gmail.com>
2024-04-28 11:53:56 +0200

    Change login form button to remove color prop
    The color prop has been removed from the Button component in the login form. The button still has the same appearance and functionality as before.

--- MY_Stock-APP_RC-24-29/stockapp-rtk/src/components/Forms/LoginForm.jsx
+++ MY_Stock-APP_RC-24-29/stockapp-rtk/src/components/Forms/LoginForm.jsx
@@ -43,7 +43,7 @@ const LoginForm = ({ values, handleChange, errors, touched, handleBlur }) => {
           error={touched.password && Boolean(errors.password)}
         />
         {!loading ? (
-          <Button variant="contained" type="submit" color='secondary.third'>
+          <Button variant="contained" type="submit">
            Sign In
           </Button>
         ) : (
