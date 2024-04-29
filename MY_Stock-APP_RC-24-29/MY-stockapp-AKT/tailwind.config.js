db84b520720019da03224864840352d3a9574be2
tarti <tartilaci@gmail.com>
2024-04-29 21:42:26 +0200

    Change index.html to use dark mode and enable dark mode in tailwind.config.js
    - Modify index.html to include 'className="dark"' in the body tag
    - Update tailwind.config.js to enable

--- MY_Stock-APP_RC-24-29/MY-stockapp-AKT/tailwind.config.js
+++ MY_Stock-APP_RC-24-29/MY-stockapp-AKT/tailwind.config.js
@@ -5,10 +5,11 @@ export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
-
     // Path to Tremor module
     "./node_modules/@tremor/**/*.{js,ts,jsx,tsx}",
+
   ],
+  darkMode: "class",
   theme: {
     transparent: 'transparent',
     current: 'currentColor',
