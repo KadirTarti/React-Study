73b97ad5345a30154827c0182620e0ca98f46f5f
tarti <tartilaci@gmail.com>
2024-04-29 11:39:43 +0200

    "Added monospace font-family to MyTextField component"
    This commit adds the `style={{fontFamily:'monospace'}}` attribute to the `TextField` component in the `MyTextField` functional component. This will result

--- MY_Stock-APP_RC-24-29/MY-stockapp-AKT/src/components/Commons/MyTextField.jsx
+++ MY_Stock-APP_RC-24-29/MY-stockapp-AKT/src/components/Commons/MyTextField.jsx
@@ -1,7 +1,7 @@
 import TextField from "@mui/material/TextField";
 import React from "react";
 const MyTextField = (props) => {
-  return <TextField {...props} />;
+  return <TextField style={{fontFamily:'monospace'}} {...props} />;
 };
 
 export default MyTextField;
