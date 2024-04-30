9e960327cb1cd18c388dff7e72d7452209718479
tarti <tartilaci@gmail.com>
2024-04-30 15:28:38 +0200

    "Updated AppRouter to set Home page as the default route, commented out previous default route of Login page"

--- BLOG_APP/src/router/AppRouter.jsx
+++ BLOG_APP/src/router/AppRouter.jsx
@@ -4,12 +4,14 @@ import Login from "../pages/Login";
 import Register from "../pages/Register";
 import PrivateRouter from "./PrivateRouter";
 import Dashboard from "../pages/Dashboard";
+import Home from "../pages/Home";
 
 const AppRouter = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Login />} />
+      <Route path="/" element={<Home/>} />
+        {/* <Route path="/" element={<Login />} /> */}
         <Route path="register" element={<Register />} />
         <Route path="stock" element={<PrivateRouter />}>
           <Route path="" element={<Dashboard />} />
