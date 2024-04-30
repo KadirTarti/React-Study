c0b96b60d3feeadf0ebe7a1f15294cbe32d39be4
tarti <tartilaci@gmail.com>
2024-04-30 11:09:01 +0200

    "Added email and password input fields to LoginForm component with formik and material-ui; added show/hide password functionality"
    This commit adds the following changes to the LoginForm.jsx component:
    
    1. Added email

--- MY-BlogApp/src/components/forms/LoginForm.jsx
+++ MY-BlogApp/src/components/forms/LoginForm.jsx
@@ -11,11 +11,64 @@ export const loginScheme = object({
  const LoginForm = ({ values, handleChange, errors, touched, handleBlur }) => {
     const { loading } = useSelector(state => state.auth);// storeda yaptığımız fetchStart işlemini kullanmış olduk.
 
-
+    const [showPassword, setShowPassword] = useState(false);
+  
+    const handleClickShowPassword = () => {
+      setShowPassword(!showPassword);
+    };
+  
 
   return (
-    <div>LoginForm</div>
-  )
+    <Form>
+    <Box sx={{ display: "flex", flexDirection: "column", gap: 1, width:'60%', margin:'auto'  }}>
+        <TextField
+          label="Email"
+          name="email"
+          id="email"
+          type="email"
+          variant="outlined"
+          style={{fontFamily:'monospace'}}
+          value={values.email}
+          onChange={handleChange}
+          onBlur={handleBlur}
+          helperText={touched.email && errors.email}
+          error={touched.email && Boolean(errors.email)}
+        />
+        <TextField
+          label="Password"
+          name="password"
+          id="password"
+          type={showPassword ? 'text' : 'password'}
+          variant="outlined"
+          style={{fontFamily:'monospace'}}
+          value={values.password}
+          onChange={handleChange}
+          onBlur={handleBlur}
+          helperText={touched.password && errors.password}
+          error={touched.password && Boolean(errors.password)}
+          InputProps={{
+              endAdornment: (
+                <InputAdornment position="end">
+                 <IconButton
+                    aria-label="toggle password visibility"
+                    onClick={handleClickShowPassword}
+                 >
+                    {showPassword ? <VisibilityOff /> : <Visibility />}
+                 </IconButton>
+                </InputAdornment>),
+            }}   
+        />
+        {!loading ? (
+          <Button variant="contained" type="submit" style={{fontFamily:'monospace'}}>
+           Sign In
+          </Button>
+        ) : (
+          <Button variant="contained" disabled={loading}>
+            <CircularProgress />
+          </Button>
+        )}
+        </Box>
+    </Form>
+  );
 }
-
 export default LoginForm
