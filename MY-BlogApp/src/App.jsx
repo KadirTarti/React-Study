0383be335f112ef37f77d72e56335287661390db
tarti <tartilaci@gmail.com>
2024-04-30 14:14:00 +0200

    "Remove persistor and related imports from app and store files"
    This commit removes the use of the `persistor` object from the `App.jsx` file and the related imports from `persist-gate`

--- MY-BlogApp/src/App.jsx
+++ MY-BlogApp/src/App.jsx
@@ -1,9 +1,7 @@
 import { Provider } from "react-redux";
 import { ToastContainer } from "react-toastify";
-import store, { persistor } from "./app/store";
+import store from "./app/store";
 import AppRouter from "./router/AppRouter";
-import { PersistGate } from 'redux-persist/es/integration/react';
-import { PersistGate } from 'redux-persist/integration/react'
  
 
 function App() {
@@ -12,9 +10,7 @@ function App() {
   return (
     <>
         <Provider store={store}>
-          <PersistGate loading={null} persistor={persistor}>
             <AppRouter />
-          </PersistGate>
         </Provider>
         <ToastContainer />
     </>
