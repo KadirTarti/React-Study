86aa8a9069e492a6c08d389e362ff78360652b8b
tarti <tartilaci@gmail.com>
2024-05-02 12:26:07 +0200

    Add redux-persist to persist auth state
    - Update package.json and pnpm-lock.yaml to include redux-persist
    - Import PersistGate and persistor in App.jsx
    -

--- BLOG_APP/src/App.jsx
+++ BLOG_APP/src/App.jsx
@@ -3,6 +3,8 @@ import AppRouter from "./router/AppRouter";
 import { Provider } from "react-redux";
 import store from "./app/store";
 import { ToastContainer } from "react-toastify";
+import { PersistGate } from "redux-persist/integration/react";
+import store, { persistor } from "./app/store";
 
 function App() {
   const theme = createTheme({
@@ -20,7 +22,9 @@ function App() {
     <>
       <ThemeProvider theme={theme}>
         <Provider store={store}>
+        <PersistGate loading={null} persistor={persistor}>
           <AppRouter />
+        </PersistGate>
         </Provider>
         <ToastContainer />
       </ThemeProvider>
--- BLOG_APP/src/app/store.jsx
+++ BLOG_APP/src/app/store.jsx
@@ -1,10 +1,24 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "../features/authSlice";
+import storage from "redux-persist/lib/storage"; // defaults to localStorage for web
+import {
+  persistReducer,
+  persistStore,
+} from "redux-persist";
+
+const persistConfig = {
+  key: "root",
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, authReducer);
 
 const store = configureStore({
   reducer: {
-    auth: authReducer,
+    auth: persistedReducer,
   },
   devTools: process.env.NODE_ENV !== "production",
 });
+export let persistor = persistStore(store);
+
 export default store;
