95f2a2e981ee5aeb9de45c5fb6d5b58555101891
tarti <tartilaci@gmail.com>
2024-05-04 23:18:34 +0200

    "Update authSlice by adding commented out code for customized middleware with serializableCheck set to false"
    Explanation:
    The code diff shows the addition of commented out code for customized middleware in the `authSlice`

--- react-vite-BLOGAPP/src/features/authSlice.js
+++ react-vite-BLOGAPP/src/features/authSlice.js
@@ -1,10 +1,16 @@
 import {createSlice} from '@reduxjs/toolkit'
+// import { getDefaultMiddleware } from '@reduxjs/toolkit';
+
+// const customizedMiddleware = getDefaultMiddleware({
+//     serializableCheck: false
+//   })
 
 export const authSlice = createSlice({
     name: 'auth',
     initialState: {
         token: ''
     },
+    
     reducers: {
         setToken: (state, action) => {
             state.token = action.payload
