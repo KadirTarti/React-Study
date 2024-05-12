524de5e91a5590ac92075498805c610ec8329724
tarti <tartilaci@gmail.com>
2024-05-12 23:31:45 +0200

    "Update Card component with Material Tailwind UI components"
    This commit updates the Card component in the react-vite-BLOGAPP project by replacing the previous implementation with the Material Tailwind UI components. The new implementation includes the

--- react-vite-BLOGAPP/src/pages/Card.jsx
+++ react-vite-BLOGAPP/src/pages/Card.jsx
@@ -1,5 +1,17 @@
+import {
+  Card,
+  CardHeader,
+  CardBody,
+  CardFooter,
+  Typography,
+  Button,
+  Tooltip,
+  IconButton,
+} from "@material-tailwind/react";
+
 const Card = ({ blog }) => {
   return (
+
     <div className="card">
       <img src={blog.image} alt={blog.title} />
       <h3>{blog.title}</h3>
