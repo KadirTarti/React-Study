00542005188ed06ef2548e7c9a2cbfea6fb6ad35
tarti <tartilaci@gmail.com>
2024-05-12 22:23:13 +0200

    "Updated home.cy.js to simplify selection of recipe type in home page"
    This commit removes the explicit search for the 'lunch' option in the recipe type selection dropdown and replaces it with a direct click on

--- RC-17-18-RecipeAppProject/cypress/e2e/home.cy.js
+++ RC-17-18-RecipeAppProject/cypress/e2e/home.cy.js
@@ -11,7 +11,8 @@ describe('home', () => {
         cy.url().should("include", "/home");
         cy.get("[data-test='recipeHeader']").contains("Healthy").should("be.visible")
         cy.get("[data-test='menuOpenButton']").click();
-        cy.get("[data-test='selectType']").contains("lunch").should("be.visible").click({force: true})
+        // cy.get("[data-test='selectType']").contains("lunch").should("be.visible").click({force: true})
+        cy.get("[data-test='selectType']").should("be.visible").click({force: true})
         cy.get("[data-test='homeSearch']").type("r")
         cy.get("[data-test='homeSearchBtn']").click({ force: true });
         cy.get("[data-test='cardHeader']").should("be.visible")
