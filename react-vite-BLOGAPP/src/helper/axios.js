81da6be6ddff7efb099888a6f54ac35b8641a91b
tarti <tartilaci@gmail.com>
2024-05-21 16:46:31 +0200

    delete files

--- react-vite-BLOGAPP/src/helper/axios.js
+++
@@ -1,7 +0,0 @@
-import axios from "axios";
-
-export default axios.create({
-    baseURL: 'https://38103.fullstack.clarusway.com',
-    timeout: 5000,
-    headers: {'X-Custom-Header': 'foobar'}
-});
