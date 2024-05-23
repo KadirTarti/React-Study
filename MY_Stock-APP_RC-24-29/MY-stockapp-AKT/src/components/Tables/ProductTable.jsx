7e8888f7153b1a7ee34410e7e4294d934bec81fb
tarti <tartilaci@gmail.com>
2024-05-23 10:14:27 +0200

    "Removed EditIcon and GridActionsCellItem imports, and simplified the renderCell function in ProductTable component."

--- MY_Stock-APP_RC-24-29/MY-stockapp-AKT/src/components/Tables/ProductTable.jsx
+++ MY_Stock-APP_RC-24-29/MY-stockapp-AKT/src/components/Tables/ProductTable.jsx
@@ -7,11 +7,8 @@ import {
 import { btnStyle } from "../../styles/globalStyle";
 import DataTable from "../Commons/DataTable";
 import Loading from "../Commons/Loading";
-import EditIcon from "@mui/icons-material/Edit"; 
-import { GridActionsCellItem } from "@mui/x-data-grid";
 
-
-export default function ProductTable({ handleOpen, setInitialState }) {
+export default function ProductTable() {
   const { data: products, isLoading } = useGetProductsQuery();
   const [deleteProduct] = useDeleteProductMutation();
   const columns = [
@@ -72,30 +69,12 @@ export default function ProductTable({ handleOpen, setInitialState }) {
       sortable: false,
       minWidth: 40,
       flex: 1,
-      renderCell: ({row: {_id,categoryId,brandId,name,quantity,actions}}) => (
-        <>
-        <GridActionsCellItem
-        key={'edit'}
-          icon={<EditIcon />}
-          label="Edit"
-          onClick={() => {
-            handleOpen();
-            setInitialState({_id, categoryId, brandId, name,quantity, actions}); 
-          }}
-          sx={btnStyle}
-        />
-        <GridActionsCellItem
-          icon={<DeleteOutlineIcon />}
-          label="Delete"
-          onClick={() => deleteProduct(_id)} // Make sure to pass params.id correctly
-          sx={btnStyle}
-        />
-      </>
+      renderCell: (params) => (
         // console.log(params)
-        // <DeleteOutlineIcon
-        //   onClick={() => deleteProduct(params.id)}
-        //   sx={btnStyle}
-        // />
+        <DeleteOutlineIcon
+          onClick={() => deleteProduct(params.id)}
+          sx={btnStyle}
+        />
       ),
     },
   ];
