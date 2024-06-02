d06a5f6836f5ad02ce68c3b824b8726b9db42b1d
tarti <tartilaci@gmail.com>
2024-06-02 21:54:11 +0200

    "Updated MobileNav component with new layout, added links, and styling"

--- akt-portfolio/components/MobileNav.jsx
+++ akt-portfolio/components/MobileNav.jsx
@@ -1,48 +1,69 @@
-'use client';
+"use client";
 
-import {Sheet, SheetContent, SheetTrigger } from '@/components/ui/sheet'
-import { usePathname } from 'next/navigation'; 
-import Link from 'next/link';
-import {CiMenuFries} from 'react-icons/ci'
-
+import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
+import { usePathname } from "next/navigation";
+import Link from "next/link";
+import { CiMenuFries } from "react-icons/ci";
 
 const links = [
   {
-    name: 'home',
-    path: '/'
+    name: "home",
+    path: "/",
   },
   {
-    name: 'services',
-    path: '/services'
+    name: "services",
+    path: "/services",
   },
   {
-    name: 'resume',
-    path: '/resume'
+    name: "resume",
+    path: "/resume",
   },
   {
-    name: 'work',
-    path: '/work'
+    name: "work",
+    path: "/work",
   },
   {
-    name: 'contact',
-    path: '/contact'
+    name: "contact",
+    path: "/contact",
   },
-]
+];
 
 const MobileNav = () => {
   const pathname = usePathname();
 
+  return (
+    <Sheet>
+      <SheetTrigger className="flex justify-center items-center">
+        <CiMenuFries className="text-[32px] text-yellow-500" />
+      </SheetTrigger>
+      <SheetContent className="flex flex-col">
+        <div className="mt-32 mb-40 text-center text-2xl">
+          <Link href="/">
+            <h1 className="text-4xl font-semibold">
+              <span className="text-amber-300">»</span> A K T
+            </h1>
+          </Link>
+        </div>
 
-  return <Sheet>
-  <SheetTrigger className='flex justify-center items-center'>
-  <CiMenuFries className='text-[32px] text-yellow-500'/>
-  </SheetTrigger>
-  <SheetContent className='flex flex-col'>
+        <nav className="flex flex-col justify-center items-center gap-8">
+          {links.map((link, index) => {
+            return (
+              <Link
+                key={index}
+                href={link.path}
+                className= {`${
+                  link.path === pathname && 
+                "text-amber-300 border-b-2 border-amber-300"
+                } "text-xl capitalize hover:text-amber-300 transition-all`}
+              >
+                {link.name}
+              </Link>
+            );
+          })}
+        </nav>
+      </SheetContent>
+    </Sheet>
+  );
+};
 
-  <div className="logo">LOGO</div>
-
-  </SheetContent>
-      </Sheet>
-}
-
-export default MobileNav
+export default MobileNav;
