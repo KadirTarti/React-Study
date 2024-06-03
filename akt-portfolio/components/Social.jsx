f8ef5db2a0f0012239e45ad075594bfd88df0f55
tarti <tartilaci@gmail.com>
2024-06-03 15:44:22 +0200

    "Updated Social component to use Lucide React icons, Next Link, and React Icons, with a new socialContacts array and mapping to render icons with links."

--- akt-portfolio/components/Social.jsx
+++ akt-portfolio/components/Social.jsx
@@ -1,8 +1,24 @@
-import React from 'react'
+import { icons } from 'lucide-react'
+import Link from 'next/link'
+import {FaGithub, FaLinkedinIn, FaXing} from 'react-icons/fa'
 
-const Social = () => {
+const socialContacts = [
+    {icon: <FaGithub/>, path: ""},
+    {icon: <FaLinkedinIn/>, path: ""},
+    {icon: <FaXing/>, path: ""},
+]
+
+const Social = ({containerStyles, iconStyles}) => {
   return (
-    <div>Social</div>
+    <div className={containerStyles}>
+    {socialContacts.map((item, index)=> {
+        return (
+            <Link key={index} href={item.path} className={iconStyles}>
+                {item.icon}
+            </Link>
+        )
+    })}
+    </div>
   )
 }
 
