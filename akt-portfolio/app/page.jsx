7260ec0fe3cf12a26188414f94ede2dfb58d9f33
tarti <tartilaci@gmail.com>
2024-06-03 15:14:14 +0200

    "Update globals.css with new typography styles, modify page.jsx to add hero section with button and photo, and adjust button styles in button.jsx"

--- akt-portfolio/app/page.jsx
+++ akt-portfolio/app/page.jsx
@@ -1,6 +1,28 @@
+import { Button } from '@/components/ui/button';
+import { FiDownload} from 'react-icons/fi';
 
-export default function Home() {
+const Home = () => {
   return (
-    <main>homepage</main>
-  );
+    <section>
+    <div className="container mx-auto h-full">Homepage</div>
+    <div className='flex flex-col xl:flex-row items-center justify-between xl:pt-8 xl:pb-24'>
+      <div className='text-center xl:text-left'>
+      <span className='text-xl'>Software Developer</span>
+      <h1 className='h1'>
+        Hello! I'm <br/> <span className='text-amber-400'>Abdulkadir TARTILACI</span>
+      </h1>
+      <p className='max-w-[500px] mb-9 text-white/80'>I am a Software Developer working mainly on frontend.
+      </p>
+      <div>
+        <Button></Button>
+      </div>
+      </div>
+
+
+      <div>photo</div>
+    </div>
+    </section>
+  )
 }
+
+export default Home;
--- akt-portfolio/components/ui/button.jsx
+++ akt-portfolio/components/ui/button.jsx
@@ -5,7 +5,7 @@ import { cva } from "class-variance-authority";
 import { cn } from "@/lib/utils"
 
 const buttonVariants = cva(
-  "inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-white transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-slate-950 focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 dark:ring-offset-slate-950 dark:focus-visible:ring-slate-300",
+  "inline-flex items-center justify-center whitespace-nowrap rounded-full text-base font-semibold ring-offset-white transition-colors",  
   {
     variants: {
       variant: {
@@ -20,10 +20,9 @@ const buttonVariants = cva(
         link: "text-slate-900 underline-offset-4 hover:underline dark:text-slate-50",
       },
       size: {
-        default: "h-10 px-4 py-2",
-        sm: "h-9 rounded-md px-3",
-        lg: "h-11 rounded-md px-8",
-        icon: "h-10 w-10",
+        default: "h-[40px] px-6",
+        md: "h-[46px] px-6",
+        lg: "h-[52px] px-8 text-sm uppercase tracking-[2px]",
       },
     },
     defaultVariants: {
