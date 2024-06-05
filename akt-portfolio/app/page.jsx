2a69b447c556848f4637ab8dd45038325fd90d7b
tarti <tartilaci@gmail.com>
2024-06-05 22:25:09 +0200

    "Added Photo component and updated Home component layout and text"

--- akt-portfolio/app/page.jsx
+++ akt-portfolio/app/page.jsx
@@ -3,18 +3,19 @@ import { FiDownload} from 'react-icons/fi';
 
 //components
 import Social from '@/components/Social';
+import Photo from '@/components/Photo';
 
 const Home = () => {
   return (
     <section className='h-full'>
     <div className="container mx-auto h-full">
     <div className='flex flex-col xl:flex-row items-center justify-between xl:pt-8 xl:pb-24'>
-      <div className='text-center xl:text-left'>
+      <div className='text-center xl:text-left order-2 xl:order-none'>
       <span className='text-xl'>Software Developer</span>
       <h1 className='h1'>
         Hello! I'm <br/> <span className='text-amber-400'>Abdulkadir TARTILACI</span>
       </h1>
-      <p className='max-w-[500px] mb-9 text-white/80'>I am a Software Developer working mainly on frontend.
+      <p className='max-w-[500px] mb-9 text-white/80'>I am a Software Developer and working mainly on frontend. I love design!
       </p>
       <div className='flex flex-col xl:flex-row items-center gap-8'>
         <Button variant='outline' size='lg' className='uppercase flex items-center gap-2 text-amber-300 bg-transparent hover:text-white hover:bg-pink-900'>
@@ -28,7 +29,9 @@ const Home = () => {
       </div>
 
 
-      <div>photo</div>
+      <div className='order-1 xl:order-none mb-8 xl:mb-0'>
+      <Photo/>
+      </div>
     </div>
     </div>
     </section>
