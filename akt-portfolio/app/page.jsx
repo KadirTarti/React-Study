9b043125e268a4b06c4af616535b48ea0d4c0896
tarti <tartilaci@gmail.com>
2024-06-08 21:51:56 +0200

    "Added Stats component to page.jsx and installed react-countup dependency"

--- akt-portfolio/app/page.jsx
+++ akt-portfolio/app/page.jsx
@@ -4,6 +4,7 @@ import { FiDownload} from 'react-icons/fi';
 //components
 import Social from '@/components/Social';
 import Photo from '@/components/Photo';
+import Stats from '@/components/Stats';
 
 const Home = () => {
   return (
@@ -34,6 +35,7 @@ const Home = () => {
       </div>
     </div>
     </div>
+    <Stats/>
     </section>
   )
 }
---
+++ akt-portfolio/components/Stats.jsx
@@ -0,0 +1,16 @@
+"use client";
+
+import CountUp from "react-countup/build/CountUp";
+
+const stats = [
+  { num: 14, text: "Years of work experience" },
+  { num: 1, text: "Years of IT experience" },
+  { num: 12, text: "Years of experience" },
+  { num: 12, text: "Years of experience" },
+];
+
+const Stats = () => {
+  return <div>Stats</div>;
+};
+
+export default Stats;
