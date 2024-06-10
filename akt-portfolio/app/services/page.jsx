e9b967498fe39e54991b175af6aca82eefa3c75d
tarti <tartilaci@gmail.com>
2024-06-10 16:56:36 +0200

    "Added 'use client' directive, imported components, and defined services array in page.jsx"

--- akt-portfolio/app/services/page.jsx
+++ akt-portfolio/app/services/page.jsx
@@ -1,3 +1,43 @@
+'use client'
+
+import {BsArrowDonwRight} from 'react-icons/bs'
+import Link from 'next/link'
+import { Description } from '@radix-ui/react-dialog'
+
+
+const services = [
+  {
+    num: '01',
+    title: 'Web Development',
+    description: 'descriptien should come here.',
+    href: '',
+  },
+  {
+    num: '02',
+    title: 'UI Design',
+    description: 'descriptien should come here.',
+    href: '',
+  },
+  {
+    num: '03',
+    title: '',
+    description: 'descriptien should come here.',
+    href: '',
+  },
+  {
+    num: '01',
+    title: 'Web Development',
+    description: 'descriptien should come here.',
+    href: '',
+  },
+  {
+    num: '01',
+    title: 'Web Development',
+    description: 'descriptien should come here.',
+    href: '',
+  },
+]
+
 
 const Services = () => {
   return (
