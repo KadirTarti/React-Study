11c45ac65d7e321fad54e44615573c2b9352891e
tarti <tartilaci@gmail.com>
2024-06-19 00:28:07 +0200

    "Added new project to MyProjects page, updated JSX structure and imported new GIF asset"

--- akt-portfolio/app/myprojects/page.jsx
+++ akt-portfolio/app/myprojects/page.jsx
@@ -14,6 +14,7 @@ import Link from 'next/link'
 import Image from 'next/image'
 
 import gifOne from '../../public/assets/Inventorymaster.gif'
+import gifTwo from '../../public/assets/ShoppingApp.gif'
 
 
 const projects = [
@@ -25,17 +26,34 @@ const projects = [
     stack: [{name: 'React.js'}, {name: 'Redux'}, {name: 'MUI'}, {name: 'Tailwind'}, {name: 'Formik & Yup'}],
     image:  {gifOne},
     live: 'https://akt-inventory-master.netlify.app/',
-    github: 'https://github.com/KadirTarti/inventory-master',
-    
-
-    
-  }
+    github: 'https://github.com/KadirTarti/inventory-master',    
+  },
+  {
+    num: '02',
+    category: 'Fullstack',
+    title: 'C-link E-Markt',
+    description: 'desc is here',
+    stack: [{name: 'React.js'}, {name: 'Bootstrap'}, {name: 'MockAPI'}, {name: 'Navigation'}],
+    image:  {gifTwo},
+    live: 'https://shopping-app-akt.netlify.app/',
+    github: 'https://github.com/KadirTarti/ShoppingApp',    
+  },
 ]
 
 
 const MyProjects = () => {
+
+  const [project, setProject] = useState(projects[0]);
+
+
   return (
-    <div>MyProjects</div>
+    <motion.section initial={{opacity: 0}} animate={{opacity: 1}} className='min-h-[80vh] flex flex-col justify-center py-12 xl:px-0'>
+    <div className="container mx-auto">
+
+      
+    </div>
+
+    </motion.section>
   )
 }
 
