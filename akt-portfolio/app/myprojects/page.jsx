f7e461c0214d3a68725507dffd7195b6ef73fb9d
tarti <tartilaci@gmail.com>
2024-06-19 00:21:36 +0200

    "Added Swiper library and implemented it in MyProjects page with accompanying imports and configurations"

--- akt-portfolio/app/myprojects/page.jsx
+++ akt-portfolio/app/myprojects/page.jsx
@@ -1,3 +1,37 @@
+'use client';
+
+import {motion} from 'framer-motion'
+import React, { useState } from 'react';
+
+import {Swiper, SwiperSlide} from 'swiper/react'
+import 'swiper/css'
+
+import { BsArrowUpRight, BsGithub } from 'react-icons/bs';
+
+import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from '@radix-ui/react-tooltip';
+
+import Link from 'next/link'
+import Image from 'next/image'
+
+import gifOne from '../../public/assets/Inventorymaster.gif'
+
+
+const projects = [
+  {
+    num: '01',
+    category: 'Fullstack',
+    title: 'Inventory Master',
+    description: 'desc is here',
+    stack: [{name: 'React.js'}, {name: 'Redux'}, {name: 'MUI'}, {name: 'Tailwind'}, {name: 'Formik & Yup'}],
+    image:  {gifOne},
+    live: 'https://akt-inventory-master.netlify.app/',
+    github: 'https://github.com/KadirTarti/inventory-master',
+    
+
+    
+  }
+]
+
 
 const MyProjects = () => {
   return (
