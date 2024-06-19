f4b17ad5be7929d1ed9e2fe163035bc64a1dc9d7
tarti <tartilaci@gmail.com>
2024-06-19 14:33:11 +0200

    "Updated SliderButtons component: reformatted code, added onClick events to buttons, and removed trailing newline."

--- akt-portfolio/components/helpers/SliderButtons.jsx
+++ akt-portfolio/components/helpers/SliderButtons.jsx
@@ -1,23 +1,22 @@
-'use client';
+"use client";
 
-import { useSwiper} from 'swiper/react'
-import {PiCaretDoubleLeftBold, PiCaretDoubleRightBold} from 'react-icons/pi'
-import { icons } from 'lucide-react';
+import { useSwiper } from "swiper/react";
+import { PiCaretDoubleLeftBold, PiCaretDoubleRightBold } from "react-icons/pi";
+import { icons } from "lucide-react";
 
 const SliderButtons = (containerStyles, buttonStyles, iconsStyles) => {
-    const swiper = useSwiper();
+  const swiper = useSwiper();
 
   return (
     <div className={containerStyles}>
-    <button className={buttonStyles}>
-        <PiCaretDoubleLeftBold className={iconsStyles}/>
-    </button>
-    <button className={buttonStyles}>
-        <PiCaretDoubleRightBold className={iconsStyles}/>
-    </button>
-    
+      <button className={buttonStyles} onClick={() => swiper.slidePrev()}>
+        <PiCaretDoubleLeftBold className={iconsStyles} />
+      </button>
+      <button className={buttonStyles} onClick={() => swiper.slideNext()}>
+        <PiCaretDoubleRightBold className={iconsStyles} />
+      </button>
     </div>
-  )
-}
+  );
+};
 
-export default SliderButtons
+export default SliderButtons;
